5e37562c75a9f03263b85adc82b21c245a8f8259
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-04-24 18:37:53 +0800

    Added footer render from drupal menu

--- src/Component/Footer/FooterComponent.php
+++ src/Component/Footer/FooterComponent.php
@@ -7,6 +7,37 @@ use App\Plugins\ComponentWidget\ComponentWidgetInterface;
 class FooterComponent implements ComponentWidgetInterface
 {
     /**
+     * @var App\Fetcher\Drupal\MenuFetcher
+     */
+    private $menuFetcher;
+
+    /**
+     * @var App\Player\PlayerSession
+     */
+    private $playerSession;
+
+    /**
+     *
+     */
+    public static function create($container)
+    {
+        return new static(
+            $container->get('menu_fetcher'),
+            $container->get('player_session')
+        );
+    }
+
+    /**
+     * Public constructor
+     */
+    public function __construct($menuFetcher, $playerSession)
+    {
+        $this->menuFetcher = $menuFetcher;
+        $this->playerSession = $playerSession;
+    }
+
+
+    /**
      * Defines the template path
      *
      * @return string
@@ -24,6 +55,14 @@ class FooterComponent implements ComponentWidgetInterface
     public function getData()
     {
         $data = [];
+        $data['is_logged_in'] = $this->playerSession->isLogin();
+
+        try {
+            $data['footer_menu'] = $this->menuFetcher
+                ->getMultilingualMenu('mobile-footer');
+        } catch (\Exception $e) {
+            $data['footer_menu'] = [];
+        }
 
         $data['copyright'] = 'Copyright';
 
