5041bd04e756d80caeae5711f81a3f4af949eda8
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-04-24 10:44:18 +0800

    Added working login

--- src/Component/Header/HeaderComponentController.php
+++ src/Component/Header/HeaderComponentController.php
@@ -10,8 +10,42 @@ class HeaderComponentController
     /**
      *
      */
-    public function submit($request, $response)
+    public static function create($container)
     {
+        return new static(
+            $container->get('player_session')
+        );
+    }
+
+    /**
+     * Public constructor
+     */
+    public function __construct($playerSession)
+    {
+        $this->playerSession = $playerSession;
+    }
+
+    /**
+     *
+     */
+    public function authenticate($request, $response)
+    {
+        $data = [];
+        $body = $request->getParsedBody();
+
+        if (!empty($body['username']) && !empty($body['password'])) {
+            $username = $body['username'];
+            $password = $body['password'];
+
+            try {
+                $data['success'] = $this->playerSession->login($username, $password);
+            } catch (\Exception $e) {
+                $data['code'] = $e->getCode();
+                $data['reason'] = $e->getMessage();
+            }
+        }
+
+        return $data;
     }
 
     /**
@@ -19,5 +53,14 @@ class HeaderComponentController
      */
     public function logout($request, $response)
     {
+        $data = [];
+
+        try {
+            $this->playerSession->logout();
+        } catch (\Exception $e) {
+            // do nothing
+        }
+
+        return $data;
     }
 }
