3e4b53d638125fb69357d5bcd591da6f8cbeeb78
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-04-25 17:36:01 +0800

    Added async for announcement

--- src/Component/Announcement/AnnouncementBar/script.ts
+++ src/Component/Announcement/AnnouncementBar/script.ts
@@ -1,35 +1,37 @@
 import * as utility from '@core/assets/js/components/utility';
-import {ComponentManager, ComponentInterface} from '@plugins/ComponentWidget/asset/component';
 import Storage from '@core/assets/js/components/utils/storage';
 
-export class AnnouncementBarComponent implements ComponentInterface {
-	private storage: Storage;    
+import {ComponentManager, ComponentInterface} from '@plugins/ComponentWidget/asset/component';
 
-	constructor() {
-		this.storage = new Storage();
-	}
+export class AnnouncementBarComponent implements ComponentInterface {
+    private storage: Storage;
+
+    constructor() {
+        this.storage = new Storage();
+    }
 
     onLoad(element: HTMLElement, attachments: {}) {
-    	this.activateAnnouncementBar(element);
-    	this.bindDismissButton(element);
+        this.activateAnnouncementBar(element);
+        this.bindDismissButton(element);
     }
 
     onReload(element: HTMLElement, attachments: {}) {
-    	console.log('test');
-    	this.activateAnnouncementBar(element);
-    	this.bindDismissButton(element);
+        this.activateAnnouncementBar(element);
+        this.bindDismissButton(element);
     }
 
     /**
      * Show announcement bar     
      */
     private activateAnnouncementBar(element) {
-    	let readItems = new Array();
-    	let activeItem = element.querySelector('.announcement-list');
-        if (activeItem != null) {
+        let readItems = [];
+        let activeItem = element.querySelector('.announcement-list');
+
+        if (activeItem) {
             readItems = this.getReadItems();
             activeItem = activeItem.getAttribute('data');
-            if (readItems.length > 0 && readItems.indexOf(activeItem) >-1) {
+
+            if (readItems.length > 0 && readItems.indexOf(activeItem) > -1) {
                 element.querySelector('.mount-announcement').style.display = 'none';
             }
         }
@@ -38,44 +40,40 @@ export class AnnouncementBarComponent implements ComponentInterface {
     /**
      * Mark announcement as read     
      */
-    private bindDismissButton(element) {    	
-    	utility.delegate(document, '.btn-dismiss', 'click', (event, src) => {
+    private bindDismissButton(element) {
+        utility.delegate(document, '.btn-dismiss', 'click', (event, src) => {
             event.preventDefault();
-    		
-	    	let activeItem = element.querySelector('.announcement-list').getAttribute('data');
 
-	    	this.setReadItems(activeItem);
-	    	ComponentManager.refreshComponent('announcement_bar', (response) => {
-				
-            });
+            let activeItem = element.querySelector('.announcement-list').getAttribute('data');
+            this.setReadItems(activeItem);
+
+            ComponentManager.refreshComponent('announcement_bar');
         }, true);
-    	
-
     }
 
     /**
      *  Get all Read Items
      */
     private getReadItems() {
-    	let data = [];
-    	if (this.storage.get('ReadItems')) {
-    		data = JSON.parse(this.storage.get('ReadItems'));
-    	}
+        let data = [];
 
-    	return data;
+        if (this.storage.get('ReadItems')) {
+            data = JSON.parse(this.storage.get('ReadItems'));
+        }
+
+        return data;
     }
 
-	/**
-	 * Mark announcement as Read
-	 * 
-	 */
+    /**
+     * Mark announcement as Read
+     * 
+     */
     private setReadItems(newItem) {
-    	let prevReadItems = [];
+        let prevReadItems = [];
 
-    	prevReadItems = this.getReadItems();
-    	prevReadItems.push(newItem);
+        prevReadItems = this.getReadItems();
+        prevReadItems.push(newItem);
 
-    	this.storage.set('ReadItems', JSON.stringify(prevReadItems));
-
+        this.storage.set('ReadItems', JSON.stringify(prevReadItems));
     }
 }
