f74ee4985e18c97fe37288eb498d682a9073794c
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-04-25 14:05:16 +0800

    Remove unused settings

--- app/settings.php
+++ app/settings.php
@@ -7,17 +7,10 @@ require APP_ROOT . '/core/app/settings.php';
 $settings['settings']['debug'] = true;
 $settings['settings']['configurations']['inheritance'] = false;
 
-
 $settings['settings']['renderer']['template_path']['app'] = __DIR__ . '/../';
 $settings['settings']['renderer']['template_path']['site'] = __DIR__ . '/../templates';
 $settings['settings']['renderer']['template_path']['component'] = __DIR__ . '/../src/Component';
 
 $settings['settings']['product'] = 'mobile-entrypage';
-// $settings['settings']['product_url'] = ['demo'];
 
 $settings['settings']['asset']['prefixed'] = false;
-// $settings['settings']['asset']['product_prefix'] = 'demo';
-
-// Tracking
-
-$settings['settings']['tracking']['enable'] = true;
