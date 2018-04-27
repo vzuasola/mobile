952714fd2023e0cef6ec0576076b68bb103d189b
Leandro Vicario <leandro.vicario@bayviewtechnology.com>
2018-04-27 09:50:20 +0800

    WBC-693 Added and customize password mask

---
+++ assets/script/components/password-mask.js
@@ -0,0 +1,32 @@
+import * as utility from '@core/assets/js/components/utility';
+
+/**
+ * Password Mark/Unmask
+ *
+ * @param Node input password element
+ */
+export function passwordMask(input) {
+    if (input && utility.hasClass(input, "password-mask-enabled")) {
+        var icon = utility.findSibling(input, '.password-mask-icon');
+
+        // show icon
+        utility.removeClass(icon, "hidden");
+
+        utility.addEventListener(icon, "click", changeType.bind(null, input));
+    }
+}
+
+/**
+ * Change input type
+ */
+function changeType(input) {
+    var icon = utility.findSibling(input, '.password-mask-icon');
+
+    if (input.type === "password") {
+        input.setAttribute("type", "text");
+    } else {
+        input.setAttribute("type", "password");
+    }
+
+    utility.toggleClass(icon, "unmask");
+}
