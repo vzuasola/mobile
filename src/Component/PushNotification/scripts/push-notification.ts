353fbe727bd2f2162749facd02b45d869e781078
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-04-27 16:23:19 +0800

    Pushnx fix

--- src/Component/PushNotification/scripts/push-notification.ts
+++ src/Component/PushNotification/scripts/push-notification.ts
@@ -1,33 +1,36 @@
-import pushNotification from "@core/assets/js/components/push-notification";
+import PushNX from "@core/assets/js/components/push-notification";
 
-import pushnxExpiredMessage from "./../handlebars/pushnx/expired.message.handlebars";
-import pushnxExpirationDate from "./../handlebars/pushnx/expiration.date.handlebars";
-import pushnxBody from "./../handlebars/pushnx/body.handlebars";
-import pushnxAction from "./../handlebars/pushnx/action.handlebars";
-import pushnxMessage from "./../handlebars/pushnx/message.handlebars";
-import pushnxDismissMessage from "./../handlebars/pushnx/dismiss.message.handlebars";
+import * as expireMessageTemplate from "./../handlebars/pushnx/expired.message.handlebars";
+import * as expireDateTemplate from "./../handlebars/pushnx/expiration.date.handlebars";
+import * as bodyTemplate from "./../handlebars/pushnx/body.handlebars";
+import * as actionTemplate from "./../handlebars/pushnx/action.handlebars";
+import * as messageTemplate from "./../handlebars/pushnx/message.handlebars";
+import * as dismissTemplate from "./../handlebars/pushnx/dismiss.message.handlebars";
 
 export class PushNotification {
     private pushnx;
 
     constructor(element, attachments: {authenticated: boolean}) {
-        this.pushnx = new pushNotification({
+        this.pushnx = new PushNX({
             lang: 'en',
             islogin: attachments.authenticated,
             enable: false, // start pushnx - default value true
             modal: true, // use modal - default value true
-            template: { // override templates
-                body: pushnxBody, // body
-                action: pushnxAction, // action
-                message: pushnxMessage, // message
-                expirationDate: pushnxExpirationDate, // expiration date
-                expiredMessage: pushnxExpiredMessage, // expired error message
-                dismissAllMessage: pushnxDismissMessage // dismiss all message
-            },
             dismiss: true, // dismiss all message - default value false
             counter: true, // message counter custom event "pnxCountMessage"
             notify: true, // new message indicator custom event "pnxNewMessage"
-            action: false // bind message action buttons - default value true - you can also override by using custom event "pnxAction"
+            action: false, // bind message action buttons - default value true - you can also override by using custom event "pnxAction"
+            template: { // override templates
+                body: bodyTemplate, // body
+                action: actionTemplate, // action
+                message: messageTemplate, // message
+                expirationDate: expireDateTemplate, // expiration date
+                expiredMessage: expireMessageTemplate, // expired error message
+                dismissAllMessage: dismissTemplate // dismiss all message
+            },
         });
+
+        this.pushnx.enable();
+        this.pushnx.bindAction();
     }
 }
