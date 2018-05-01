0825a6fa922782dfb56beeb55de084d98b339d14
Glenn Fundimera <glenn.fundimera@bayviewtechnology.com>
2018-05-01 18:22:39 +0800

    initial login modal

--- src/Component/PushNotification/scripts/push-notification.ts
+++ src/Component/PushNotification/scripts/push-notification.ts
@@ -11,36 +11,94 @@ import * as messageTemplate from "./../handlebars/pushnx/message.handlebars";
 
 export class PushNotification {
     private pushnx;
+    private isready;
 
     constructor(element, attachments: {authenticated: boolean, pushnx: object}) {
-        console.log(attachments);
         this.pushnx = new PushNX({
             lang: "en",
             islogin: attachments.authenticated,
             enable: true, // start pushnx - default value true
             scrollbot: false, // use default scrollbot library
-            modal: true, // use modal - default value true
+            modal: {
+                enable: true, // default value true
+                control: false // default value true
+            },
             dismiss: true, // dismiss all message - default value false
             counter: true, // message counter custom event "pnxCountMessage"
             notify: true, // new message indicator custom event "pnxNewMessage"
-            action: true, // bind message action buttons - default value true - you can also override by using custom event "pnxAction"
+            action: false, // bind message action buttons default value true custom event "pnxAction"
             template: { // override templates
                 body: bodyTemplate, // body
                 action: actionTemplate, // action
                 message: messageTemplate, // message
                 expirationDate: expireDateTemplate, // expiration date
                 expiredMessage: expireMessageTemplate, // expired error message
-                dismissAllMessage: dismissTemplate // dismiss all message
+                dismissAllMessage: dismissTemplate, // dismiss all message
             },
-            config: attachments.pushnx
+            config: attachments.pushnx,
         });
 
         // this.pushnx.enable();
-        // this.pushnx.bindAction();
+        this.attachAction(attachments.authenticated);
+        this.listenSessionLogin();
+        this.listenSessionLogout();
+        this.listenMenu();
+    }
 
-        // listen to session
+    private attachAction(islogin: boolean) {
+        //  bind action if login
+         if (islogin) {
+             this.pushnx.bindAction();
+         }
+    }
+
+    private listenSessionLogout() {
+        // listen to session logout
         utility.listen(document, "session.logout", (event) => {
             this.pushnx.bindCloseService(); // close socket connection
         });
+    }
+
+    private listenSessionLogin() {
+        var self = this;
+
+        utility.listen(document, "session.login", (event) => {
+            self.setCookie('pnxInitialLogin', true, 7);
+            self.readyMessage();
+        });
+    }
+
+    private readyMessage() {
+        var self = this;
+        utility.listen(document, "pnxMessageReady", (event) => {
+            if (event.customData.ready) {
+                self.modalProcess(event.customData.ready);
+            }
+        });
+    }
+
+    private modalProcess(status: boolean) {
+        var initial = utility.getCookie('pnxInitialLogin');
+
+        if (initial) {
+            this.pushnx.openModal();
+            utility.removeCookie('pnxInitialLogin');
+        }
+    }
+
+    private listenMenu() {
+        var self = this;
+        var menuNotif = document.querySelector('.menu-notification');
+
+        utility.listen(menuNotif, 'click', function () {
+            self.pushnx.openModal();
+        });
+    }
+
+    private setCookie(cname: string, cvalue: boolean, days: number) {
+        var d = new Date();
+        d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 }
