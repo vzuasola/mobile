f802092b03b32f943db32414d113181fa75da42f
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-05-07 19:03:55 +0800

    Update get of balance

--- src/Component/Header/Balance/BalanceComponentController.php
+++ src/Component/Header/Balance/BalanceComponentController.php
@@ -2,12 +2,20 @@
 
 namespace App\MobileEntry\Component\Header\Balance;
 
+use App\Drupal\Config;
+
 /**
  *
  */
 class BalanceComponentController
 {
+    private $config;
+
     private $playerSession;
+
+    private $user;
+
+    private $territories;
 
     private $balance;
 
@@ -17,7 +25,10 @@ class BalanceComponentController
     public static function create($container)
     {
         return new static(
+            $container->get('config_fetcher'),
             $container->get('player_session'),
+            $container->get('user_fetcher'),
+            $container->get('territory_blocking_fetcher'),
             $container->get('balance_fetcher')
         );
     }
@@ -25,16 +36,19 @@ class BalanceComponentController
     /**
      * Public constructor
      */
-    public function __construct($playerSession, $balance)
+    public function __construct($config, $playerSession, $user, $territories, $balance)
     {
+        $this->config = $config;
         $this->playerSession = $playerSession;
+        $this->user = $user;
+        $this->territories = $territories;
         $this->balance = $balance;
     }
 
     /**
      *
      */
-    public function balances()
+    public function balances($request)
     {
         $data = [];
         $isLogin = $this->playerSession->isLogin();
@@ -43,14 +57,79 @@ class BalanceComponentController
 
         if ($isLogin) {
             try {
+                $headerConfigs = $this->config->getConfig('webcomposer_config.header_configuration');
+                $balanceMap = $headerConfigs['balance_mapping'];
+                $currencyMap = $headerConfigs['currency_balance_mapping'];
+            } catch (\Exception $e) {
+                // Do nothing
+            }
+
+            try {
+                $currency = $this->user->getPlayerDetails()['currency'];
+            } catch (\Exception $e) {
+                // Do nothing
+            }
+
+            try {
+                $territoriesMap = $this->territories->getRestrictedCountries();
+                $territory = $request->getHeader('HTTP_X_CUSTOM_LB_GEOIP_COUNTRY')[0] ?? '';
+            } catch (\Exception $e) {
+                // Do nothing
+            }
+
+            try {
                 $balances = $this->balance->getBalances()['balance'];
+
+                $balances = $this->includedBalance($balanceMap, $balances);
+                $balances = $this->currencyFilter($currency, $currencyMap, $balances);
+                $balances = $this->territoryFilter($territoriesMap, $territory, $balances);
+
                 $totalBalance = array_sum($balances);
                 $data['balance'] = number_format($totalBalance, 2, '.', ',');
             } catch (\Exception $e) {
-                $data['balance'] = 'N/A';
+                $data['message'] = $e->getMessage();
+                $data['balance'] = $headerConfigs['balance_error_text_product'] ?? 'N/A';
             }
         }
 
         return $data;
+    }
+
+    /**
+     *
+     */
+    private function includedBalance($balanceMapConfig, $balances)
+    {
+        $balanceMap = Config::parse($balanceMapConfig);
+        return array_intersect_key($balances, $balanceMap);
+    }
+
+    /**
+     *
+     */
+    private function currencyFilter($currency, $currencyMapConfig, $balances)
+    {
+        $currencyMap = Config::parseCommaDelimited($currencyMapConfig);
+        $map = [];
+        foreach ($currencyMap as $key => $currencies) {
+            if (in_array(strtoupper($currency), $currencies)) {
+                $map[$key] = $currencies;
+            }
+        }
+
+        return array_intersect_key($balances, $map);
+    }
+
+    /**
+     *
+     */
+    private function territoryFilter($territoriesMapConfig, $territory, $balances)
+    {
+        foreach ($territoriesMapConfig as $key => $territories) {
+            if (in_array($territory, $territories) && isset($balances[$key])) {
+                unset($balances[$key]);
+            }
+        }
+        return $balances;
     }
 }
