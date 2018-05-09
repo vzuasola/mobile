1fb0ed94f425bf1a1ce42166d8fd9d05e2b138d6
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-05-09 11:59:48 +0800

    Added component support for node pages

--- src/Controller/NodeController.php
+++ src/Controller/NodeController.php
@@ -2,19 +2,27 @@
 
 namespace App\MobileEntry\Controller;
 
-use App\Async\Async;
-use App\Controller\NodeController as Base;
+use App\BaseController;
 
-class NodeController extends Base
+class NodeController extends BaseController
 {
     /**
      *
      */
-    public function viewNode($request, $response, $args, $node)
+    public function view($request, $response, $args)
     {
-        $data['data'] = $node;
-        $data['title'] = $node['title'][0]['value'];
+        $path = $request->getUri()->getPath();
+        $path = trim($path, '/');
 
-        return $this->widgets->render($response, '@site/page.html.twig', $data);
+        try {
+            $node = $this->get('node_fetcher')->getNodeByAlias($path);
+        } catch (\Exception $e) {
+            throw new NotFoundException($request, $response);
+        }
+
+        $data['title'] = $node['title'][0]['value'];
+        $data['node'] = $node;
+
+        return $this->widgets->render($response, '@site/node.html.twig', $data);
     }
 }
