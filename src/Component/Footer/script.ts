f2978dabe9391ab2acbf8fd3d25381de4a28cb2c
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-05-10 11:27:31 +0800

    Added script

--- src/Component/Footer/script.ts
+++ src/Component/Footer/script.ts
@@ -1,3 +1,5 @@
+import * as utility from "@core/assets/js/components/utility";
+
 import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
 
 /**
@@ -5,10 +7,18 @@ import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
  */
 export class FooterComponent implements ComponentInterface {
     onLoad(element: HTMLElement, attachments: {}) {
-        // placeholder for on load event
+        this.downloadsVisibility(element);
     }
 
     onReload(element: HTMLElement, attachments: {}) {
-        // placeholder for on reload event
+        this.downloadsVisibility(element);
+    }
+
+    private downloadsVisibility(element) {
+        const ios = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+        if (ios) {
+            utility.addClass(element.querySelector(".app-download"), "hidden");
+        }
     }
 }
