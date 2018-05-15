8f3174edc74367b0588c40a7d779aa69c469ebc6
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-05-15 14:01:10 +0800

    Added post login integration for als

--- app/dependencies.php
+++ app/dependencies.php
@@ -3,3 +3,24 @@
 // Site specific dependencies
 
 require APP_ROOT . '/core/app/dependencies.php';
+
+$container['cookie_service'] = function ($c) {
+    $baseUrl = $c->get('request')->getUri()->getBaseUrl();
+    $hostname = $c->get('parameters')['appsvc.origin.prd'];
+
+    if (preg_match('/https?:\/\/(?<env>[a-z0-9]+)-www/', $baseUrl, $matches)) {
+        $hostname = $c->get('parameters')['appsvc.origin.' . $matches['env']];
+    }
+
+    $client = new \GuzzleHttp\Client([
+        'headers' => [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        ],
+        'base_uri' => "$hostname:50982",
+    ]);
+
+    return new \App\MobileEntry\Module\ProductIntegration\ALS\Cookie(
+        $client,
+        $c->get('logger')
+    );
+};
---
+++ src/Module/ProductIntegration/ALS/LogTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\MobileEntry\Module\ProductIntegration\ALS;
+
+/**
+ * Trait for logging cookie service
+ */
+trait LogTrait
+{
+    /**
+     * Log success requirements
+     *
+     * - Log the username and sessionToken sent in the request.
+     */
+    protected function logInfo($title, $uri, $postData, $body)
+    {
+        $this->logger->info('AppSvc ' . $title, [
+            'component' => static::class,
+            'source' => $uri,
+            'username' => $postData['username'] ?? '',
+            'sessionToken' => $postData['sessionToken'] ?? '',
+            'action' => 'Guzzle client request object',
+            'object' => '',
+            'status' => 'Successful request',
+            'response' => $body,
+        ]);
+    }
+
+    /**
+     * Log error requirements
+     *
+     * - Log the complete URL including query string.
+     * - In the case of a request in which values are sent in the body,
+     *   you must include the body in the log. The body should include all
+     *   the parameters sent in the request: playerId, sessionToken, username.
+     */
+    protected function logException($title, $uri, $postData, $e)
+    {
+        $this->logger->error('AppSvc ' . $title, [
+            'component' => static::class,
+            'source' => $uri,
+            'requestBody' => $postData,
+            'action' => '',
+            'object' => '',
+            'status' => 'Failed request',
+            'response' => $e->getCode(),
+            'exception' => $e->getMessage(),
+        ]);
+    }
+}
