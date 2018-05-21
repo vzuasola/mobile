bcbb7738ac5f8cee84c1f15d6481fe0544d9de50
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2018-05-21 13:24:33 +0800

    WIP: promotions product tag

--- src/Component/Main/Promotions/script.ts
+++ src/Component/Main/Promotions/script.ts
@@ -3,8 +3,6 @@ import * as xhr from "@core/assets/js/vendor/reqwest";
 
 import {ComponentInterface, ComponentManager} from "@plugins/ComponentWidget/asset/component";
 
-import * as promotionTemplate from "./handlebars/promotion.handlebars";
-
 import * as productFeatured from "./handlebars/svg/product-featured.handlebars";
 import * as productArcade from "./handlebars/svg/product-arcade.handlebars";
 import * as productCasinoGold from "./handlebars/svg/product-casino-gold.handlebars";
@@ -15,12 +13,12 @@ import * as productFishHunter from "./handlebars/svg/product-fish-hunter.handleb
 import * as productGames from "./handlebars/svg/product-games.handlebars";
 import * as productKeno from "./handlebars/svg/product-keno.handlebars";
 import * as productLiveDealer from "./handlebars/svg/product-live-dealer.handlebars";
-import * as productLiveChat from "./handlebars/svg/product-livechat.handlebars";
 import * as productLottery from "./handlebars/svg/product-lottery.handlebars";
 import * as productOWSports from "./handlebars/svg/product-owsports.handlebars";
 import * as productPoker from "./handlebars/svg/product-poker.handlebars";
-import * as productPromotions from "./handlebars/svg/product-promotions.handlebars";
 import * as productVirtuals from "./handlebars/svg/product-virtuals.handlebars";
+
+import * as promotionTemplate from "./handlebars/promotion.handlebars";
 
 import Dropdown from "@app/assets/script/components/dropdown";
 
@@ -35,9 +33,20 @@ export class PromotionsComponent implements ComponentInterface {
 
     constructor() {
         this.productIcons = {
-            featured: productFeatured(),
             arcade: productArcade(),
+            casinogold: productCasinoGold(),
             casino: productCasino(),
+            dafasports: productDafasports(),
+            exchange: productExchange(),
+            fishhunter: productFishHunter(),
+            games: productGames(),
+            generic: productFeatured(),
+            keno: productKeno(),
+            livedealer: productLiveDealer(),
+            lottery: productLottery(),
+            owsports: productOWSports(),
+            poker: productPoker(),
+            virtuals: productVirtuals(),
         };
     }
 
@@ -57,18 +66,24 @@ export class PromotionsComponent implements ComponentInterface {
 
     init(element) {
         this.doRequest((response) => {
+            console.log("initialized");
             const productFilter = element.querySelector(".active-filter").getAttribute("data-current-filter");
-            console.log(this.productIcons);
-            console.log(response[productFilter]);
+            // let count: number = 0;
+
+            // for (const promo of response[productFilter]) {
+            //     response[productFilter][count]["product_tag"] = this.productIcons.hasOwnProperty(promo.product)
+            //         ? this.productIcons[promo.product] : "generic";
+            //     count++;
+            // }
             element.querySelector(".promotions-body").innerHTML =
-                promotionTemplate({ promotions: response[productFilter], productIcons: this.productIcons });
+                promotionTemplate({ promotions: response[productFilter] });
         });
 
     }
 
     private activateDropdown() {
         const dropdown = new Dropdown();
-        dropdown.init();
+        dropdown.init(".dropdown-trigger", 2, true, false, false);
     }
 
     private doRequest(callback) {
@@ -84,6 +99,8 @@ export class PromotionsComponent implements ComponentInterface {
             }).fail((error, message) => {
                 // do something
             });
+        } else {
+            callback(this.promotions);
         }
     }
 
@@ -91,11 +108,21 @@ export class PromotionsComponent implements ComponentInterface {
         ComponentManager.subscribe("click", (event, src) => {
             if (utility.hasClass(src, "product-link")) {
                 event.preventDefault();
+                const prevFilter = element.querySelector(".active-filter").getAttribute("data-current-filter");
+                const prevFilterEl = element.querySelector(".filter-" + prevFilter);
+
+                // remove active previous
+                utility.removeClass(utility.findParent(prevFilterEl, "li"), "active");
+
+                // set new active filter
                 utility.addClass(utility.findParent(src, "li"), "active");
+                element.querySelector(".current-filter").innerHTML =
+                    src.getAttribute("data-product-filter-name");
+                element.querySelector(".active-filter")
+               .setAttribute("data-current-filter", src.getAttribute("data-product-filter-id"));
+
                 this.doRequest((response) => {
-                    console.log(response);
                     const productFilter = src.getAttribute("data-product-filter-id");
-                    console.log(productFilter);
 
                     element.querySelector(".promotions-body").innerHTML =
                         promotionTemplate({promotions: response[productFilter]});
@@ -108,6 +135,7 @@ export class PromotionsComponent implements ComponentInterface {
 
     private filterProductCategory(element) {
         this.doRequest((response) => {
+            console.log("filter");
             const categories = Object.keys(response);
 
             for (const productCategoryEl of element.querySelectorAll(".product-link")) {
@@ -121,7 +149,8 @@ export class PromotionsComponent implements ComponentInterface {
             // set active filter
             const currentFilter = element.querySelectorAll(".product-link")[0];
             utility.addClass(utility.findParent(currentFilter, "li"), "active");
-            element.querySelector(".current-filter").innerHTML = currentFilter.getAttribute("data-product-filter-name");
+            element.querySelector(".current-filter").innerHTML =
+                currentFilter.getAttribute("data-product-filter-name");
             element.querySelector(".active-filter")
                .setAttribute("data-current-filter", currentFilter.getAttribute("data-product-filter-id"));
         });
