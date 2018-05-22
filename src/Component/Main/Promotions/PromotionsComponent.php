8dfda2d562411abed5fabdc3647fee4d6cd4aca6
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2018-05-22 13:42:18 +0800

    fix coding standards

--- src/Component/Main/Promotions/PromotionsComponent.php
+++ src/Component/Main/Promotions/PromotionsComponent.php
@@ -59,12 +59,12 @@ class PromotionsComponent implements ComponentWidgetInterface
 
         try {
             $promoConfigs = $this->configs->getConfig('mobile_promotions.promotions_configuration');
-            $data['title'] = $promoConfigs['title'];
-            $data['filter_label'] = $promoConfigs['filter_label'];
         } catch (\Exception $e) {
-            $data['title'] = 'Promotions';
-            $data['filter_label'] = 'Filter';
+            promoConfigs = [];
         }
+
+        $data['title'] = $promoConfigs['title'] ?? 'Promotions';
+        $data['filter_label'] = $promoConfigs['filter_label'] ?? 'Filter';
 
         return $data;
     }
--- src/Component/Main/Promotions/PromotionsComponentController.php
+++ src/Component/Main/Promotions/PromotionsComponentController.php
@@ -57,58 +57,60 @@ class PromotionsComponentController
 
     public function promotions($request, $response)
     {
-
         try {
-            $promoPerProduct = [];
+            
             $promotions = $this->views->getViewById('promotions');
-            $isLogin = $this->playerSession->isLogin();
-            $isProvisioned = $this->isPlayerProvisioned();
+            
+        } catch (\Exception $e) {
+            $promotions = [];
+        }
 
-            foreach ($promotions as $promotion) {
-                $filterId = $promotion['field_product_category'][0]['field_product_filter_id'][0]['value'];
-                $availability = count($promotion['field_promo_availability']) > 1 ?
-                    $promotion['field_promo_availability'] :
-                    $promotion['field_promo_availability'][0]['value'];
+        $promoPerProduct = [];
+        $isLogin = $this->playerSession->isLogin();
+        $isProvisioned = $this->isPlayerProvisioned();
 
-                $ribbonEnable = $promotion['field_enable_disable_ribbon_tag'][0]['value'] ?? '';
-                $ribbonLabel = $promotion['field_ribbon_label'][0]['value'] ?? '';
-                $ribbonColor = $promotion['field_ribbon_background_color'][0]['color'] ?? '';
-                $ribbonTextColor = $promotion['field_ribbon_text_color'][0]['color'] ?? '';
+        foreach ($promotions as $promotion) {
+            $filterId = $promotion['field_product_category'][0]['field_product_filter_id'][0]['value'];
+            $availability = count($promotion['field_promo_availability']) > 1 ?
+                $promotion['field_promo_availability'] :
+                $promotion['field_promo_availability'][0]['value'];
 
-                $promoProperties = [
-                    'title' => $promotion['title'][0]['value'],
-                    'product' => $filterId,
-                    'ribbon_enable' => $ribbonEnable,
-                    'ribbon_label' =>  $ribbonLabel,
-                    'ribbon_bg_color' => $ribbonColor,
-                    'ribbon_text_color' => $ribbonTextColor,
-                    'more_info_text' => $this->getPromoConfigs()
-                ];
+            $ribbonEnable = $promotion['field_enable_disable_ribbon_tag'][0]['value'] ?? '';
+            $ribbonLabel = $promotion['field_ribbon_label'][0]['value'] ?? '';
+            $ribbonColor = $promotion['field_ribbon_background_color'][0]['color'] ?? '';
+            $ribbonTextColor = $promotion['field_ribbon_text_color'][0]['color'] ?? '';
 
-                if ($isLogin && ($availability == '1' || is_array($availability))) {
-                    $markIsFeatured = $promotion['field_post_mark_as_featured'][0]['value'];
+            $promoProperties = [
+                'title' => $promotion['title'][0]['value'],
+                'product' => $filterId,
+                'ribbon_enable' => $ribbonEnable,
+                'ribbon_label' =>  $ribbonLabel,
+                'ribbon_bg_color' => $ribbonColor,
+                'ribbon_text_color' => $ribbonTextColor,
+                'more_info_text' => $this->getPromoConfigs()
+            ];
 
-                    if ($promotion['field_casino_gold_only'][0]['value'] && !$isProvisioned) {
-                        continue;
-                    }
+            if ($isLogin && ($availability == '1' || is_array($availability))) {
+                $markIsFeatured = $promotion['field_post_mark_as_featured'][0]['value'];
 
-                    $promoProperties = $this->getPostLoginPromotions($promoProperties, $promotion);
-                } else {
-                    $markIsFeatured = $promotion['field_mark_as_featured'][0]['value'];
-                    $promoProperties = $this->getPreLoginPromotions($promoProperties, $promotion);
+                if ($promotion['field_casino_gold_only'][0]['value'] && !$isProvisioned) {
+                    continue;
                 }
 
-                if ($markIsFeatured) {
-                    $promoPerProduct['featured'][] = $promoProperties + ['category' => 'featured'];
-                }
-
-                $promoPerProduct[$filterId][] = $promoProperties + ['category' => $filterId];
+                $promoProperties = $this->getPostLoginPromotions($promoProperties, $promotion);
+            } else {
+                $markIsFeatured = $promotion['field_mark_as_featured'][0]['value'];
+                $promoProperties = $this->getPreLoginPromotions($promoProperties, $promotion);
             }
 
-            $data = $promoPerProduct;
-        } catch (\Exception $e) {
-            $data = [];
+            if ($markIsFeatured) {
+                $promoPerProduct['featured'][] = $promoProperties + ['category' => 'featured'];
+            }
+
+            $promoPerProduct[$filterId][] = $promoProperties + ['category' => $filterId];
         }
+
+        $data = $promoPerProduct;
 
         return $this->rest->output($response, $data);
     }
@@ -132,12 +134,11 @@ class PromotionsComponentController
     {
         try {
             $promoConfigs = $this->configs->getConfig('mobile_promotions.promotions_configuration');
-            $moreInfoText = $promoConfigs['more_info_link_text'];
         } catch (\Exception $e) {
-            $moreInfoText = 'More Info';
+            $promoConfigs = [];
         }
 
-        return $moreInfoText;
+        return $promoConfigs['more_info_link_text'] ?? 'More Info';
     }
 
     private function getPreLoginPromotions($promoProperties, $promotion)
