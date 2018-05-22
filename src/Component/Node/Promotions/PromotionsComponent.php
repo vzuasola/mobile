264da20c47aa7b995c373657ce097a3ccb0fc599
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2018-05-22 14:09:11 +0800

    Rendering of inner page

---
+++ src/Component/Node/Promotions/PromotionsComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\MobileEntry\Component\Node\Promotions;
+
+use App\Plugins\ComponentWidget\ComponentWidgetInterface;
+
+class PromotionsComponent implements ComponentWidgetInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getTemplate($options = [])
+    {
+        return '@component/Node/Blog/template.html.twig';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getData($options = [])
+    {
+        $data['node'] = $options['node'];
+
+        return $data;
+    }
+}
