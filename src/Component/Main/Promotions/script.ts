5710c74cea223812b468c004494b5f87bf07d089
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-05-23 19:04:14 +0800

    Fix promotions double call

--- src/Component/Main/Promotions/script.ts
+++ src/Component/Main/Promotions/script.ts
@@ -21,16 +21,16 @@ export class PromotionsComponent implements ComponentInterface {
         Handlebars.registerHelper("equals", function(value, compare, options) {
             if (value === compare) {
                 return options.fn(this);
-            } else {
-                return options.inverse(this);
             }
+
+            return options.inverse(this);
         });
     }
 
     onLoad(element: HTMLElement, attachments: {}) {
         this.promotions = undefined;
         this.element = element;
-        this.filterProductCategory();
+
         this.init();
         this.listenChangeDropdown();
         this.activateDropdown();
@@ -39,7 +39,7 @@ export class PromotionsComponent implements ComponentInterface {
     onReload(element: HTMLElement, attachments: {}) {
         this.promotions = undefined;
         this.element = element;
-        this.filterProductCategory();
+
         this.init();
         this.listenChangeDropdown();
         this.activateDropdown();
@@ -48,9 +48,22 @@ export class PromotionsComponent implements ComponentInterface {
 
     init() {
         this.doRequest((response) => {
-            const productFilter = this.element.querySelector(".active-filter").getAttribute("data-current-filter");
-            this.element.querySelector(".promotions-body").innerHTML =
-                promotionTemplate({promotions: response[productFilter]});
+            const filter: any = this.getDefaultFilter();
+
+            this.setActiveFilter(filter);
+            this.removeUnusedFilters(response);
+
+            if (response) {
+                if (typeof response[filter] !== "undefined") {
+                    const template = promotionTemplate({
+                        promotions: response[filter],
+                    });
+
+                    this.element.querySelector(".promotions-body").innerHTML = template;
+                }
+            }
+
+            // error handling here
         });
 
     }
@@ -72,7 +85,7 @@ export class PromotionsComponent implements ComponentInterface {
         }
     }
 
-    private doRequest(callback) {
+    private doRequest(callback, errorCallback?) {
         if (!this.promotions) {
             xhr({
                 url: Router.generateRoute("promotions", "promotions"),
@@ -83,83 +96,125 @@ export class PromotionsComponent implements ComponentInterface {
 
                 callback(response);
             }).fail((error, message) => {
-                // do something
+                if (errorCallback) {
+                    errorCallback(error, message);
+                }
             });
         } else {
-                this.promotionLoader();
-                callback(this.promotions);
+            this.promotionLoader();
+            callback(this.promotions);
         }
     }
 
     private listenChangeDropdown() {
         utility.listen(this.element, "click", (event, src) => {
             if (utility.hasClass(src, "product-link")) {
-                const prevFilter = this.element.querySelector(".active-filter").getAttribute("data-current-filter");
-                const prevFilterEl = this.element.querySelector(".filter-" + prevFilter);
+                const filter = src.getAttribute("data-product-filter-id");
 
-                // remove active previous
-                utility.removeClass(utility.findParent(prevFilterEl, "li"), "active");
-                utility.removeClass(this.element.querySelector(".active-filter"), prevFilter);
+                if (filter) {
+                    this.doRequest((response) => {
+                        this.setActiveFilter(filter);
+                        // this.removeUnusedFilters(response);
 
-                // set new active filter
-                utility.addClass(utility.findParent(src, "li"), "active");
-                this.element.querySelector(".current-filter").innerHTML =
-                    src.getAttribute("data-product-filter-name");
-                this.element.querySelector(".active-filter")
-                    .setAttribute("data-current-filter", src.getAttribute("data-product-filter-id"));
-                utility.addClass(this.element.querySelector(".active-filter"),
-                    src.getAttribute("data-product-filter-id"));
+                        if (response && typeof response[filter] !== "undefined") {
+                            const productFilter = src.getAttribute("data-product-filter-id");
+                            const template = promotionTemplate({
+                                promotions: response[productFilter],
+                            });
 
-                this.doRequest((response) => {
-                    const productFilter = src.getAttribute("data-product-filter-id");
-                    this.element.querySelector(".promotions-body").innerHTML =
-                        promotionTemplate({promotions: response[productFilter]});
-                });
-            }
-        });
-    }
+                            this.element.querySelector(".promotions-body").innerHTML = template;
+                            return;
+                        }
 
-    private filterProductCategory() {
-        this.doRequest((response) => {
-            const categories = Object.keys(response);
-            const activeCategories = [];
-
-            for (const productCategoryEl of this.element.querySelectorAll(".product-link")) {
-                // remove category filters that don't have promotion
-                if (categories.indexOf(productCategoryEl.getAttribute("data-product-filter-id")) < 0) {
-                    const disabledFilter = utility.findParent(productCategoryEl, "li");
-                    disabledFilter.remove();
+                        // error handling here
+                    });
                 }
-
-                activeCategories.push(productCategoryEl.getAttribute("data-product-filter-id"));
             }
-
-            // set active filter
-            this.setInitialActiveFilter(this.element, activeCategories);
         });
     }
 
-    private setInitialActiveFilter(element, activeCategories) {
-        const hashParam = utility.getHash(window.location.href);
-        let currentFilterId: string;
-        let currentFilter: HTMLElement;
+    private getDefaultFilter() {
+        let result: any = false;
 
-        if (hashParam && activeCategories.indexOf(hashParam) > 0) {
-            currentFilterId = hashParam;
-            currentFilter = element.querySelector(".filter-" + hashParam);
-        } else {
-            currentFilter = element.querySelectorAll(".product-link")[0];
-            if (currentFilter) {
-                currentFilterId = currentFilter.getAttribute("data-product-filter-id");
-            }
+        const filters = this.getFilters();
+        const hash = utility.getHash(window.location.href);
+
+        if (hash && filters.indexOf(hash) !== -1) {
+            result = hash;
+        } else if (filters) {
+            result = filters[0];
         }
-        utility.addClass(utility.findParent(currentFilter, "li"), "active");
-        element.querySelector(".current-filter").innerHTML =
-                    currentFilter.getAttribute("data-product-filter-name");
-        element.querySelector(".active-filter")
-            .setAttribute("data-current-filter", currentFilterId);
-        utility.addClass(element.querySelector(".active-filter"),
-             currentFilterId);
 
+        return result;
+    }
+
+    private getFilters() {
+        const filters = [];
+
+        for (const item of this.element.querySelectorAll(".product-link")) {
+            filters.push(item.getAttribute("data-product-filter-id"));
+        }
+
+        return filters;
+    }
+
+    private setActiveFilter(filter: string) {
+        if (this.getFilters().indexOf(filter) === -1) {
+            filter = this.getDefaultFilter();
+        }
+
+        // remove active previous
+
+        const prevFilter = this.element.querySelector(".active-filter").getAttribute("data-current-filter");
+        const prevFilterEl = this.element.querySelector(".filter-" + prevFilter);
+
+        if (prevFilterEl) {
+            const filterList = utility.findParent(prevFilterEl, "li");
+
+            if (filterList) {
+                utility.removeClass(utility.findParent(prevFilterEl, "li"), "active");
+            }
+
+            utility.removeClass(this.element.querySelector(".active-filter"), prevFilter);
+        }
+
+        // set current filter element
+
+        let currentFilterId: string;
+        const currentFilter = this.element.querySelector(`[data-product-filter-id="${filter}"]`);
+
+        if (currentFilter) {
+            currentFilterId = currentFilter.getAttribute("data-product-filter-id");
+        }
+
+        utility.addClass(utility.findParent(currentFilter, "li"), "active");
+
+        const currentFilterProduct = currentFilter.getAttribute("data-product-filter-name");
+        this.element.querySelector(".current-filter").innerHTML = currentFilterProduct;
+
+        this.element.querySelector(".active-filter").setAttribute(
+            "data-current-filter",
+            currentFilterId,
+        );
+
+        utility.addClass(
+            this.element.querySelector(".active-filter"),
+            currentFilterId,
+        );
+    }
+
+    private removeUnusedFilters(response) {
+        const categories = Object.keys(response);
+        const activeCategories = [];
+
+        for (const productCategoryEl of this.element.querySelectorAll(".product-link")) {
+            // remove category filters that don't have promotion
+            if (categories.indexOf(productCategoryEl.getAttribute("data-product-filter-id")) < 0) {
+                const disabledFilter = utility.findParent(productCategoryEl, "li");
+                disabledFilter.remove();
+            }
+
+            activeCategories.push(productCategoryEl.getAttribute("data-product-filter-id"));
+        }
     }
 }
