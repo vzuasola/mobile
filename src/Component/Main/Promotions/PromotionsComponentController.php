23732c6ab467be671453d459692c1d44261a92fb
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2018-05-23 09:23:27 +0800

    Fetch featured from separate api to retain sorting, fix dropdown selection upon login/logout, back button

--- src/Component/Main/Promotions/PromotionsComponentController.php
+++ src/Component/Main/Promotions/PromotionsComponentController.php
@@ -67,51 +67,8 @@ class PromotionsComponentController
             $promotions = [];
         }
 
-        $promoPerProduct = [];
-        $isLogin = $this->playerSession->isLogin();
-        $isProvisioned = $this->isPlayerProvisioned();
-
-        foreach ($promotions as $promotion) {
-            $filterId = $promotion['field_product_category'][0]['field_product_filter_id'][0]['value'];
-            $availability = count($promotion['field_promo_availability']) > 1 ?
-                $promotion['field_promo_availability'] :
-                $promotion['field_promo_availability'][0]['value'];
-
-            $ribbonEnable = $promotion['field_enable_disable_ribbon_tag'][0]['value'] ?? '';
-            $ribbonLabel = $promotion['field_ribbon_label'][0]['value'] ?? '';
-            $ribbonColor = $promotion['field_ribbon_background_color'][0]['color'] ?? '';
-            $ribbonTextColor = $promotion['field_ribbon_text_color'][0]['color'] ?? '';
-
-            $promoProperties = [
-                'title' => $promotion['title'][0]['value'],
-                'product' => $filterId,
-                'ribbon_enable' => $ribbonEnable,
-                'ribbon_label' =>  $ribbonLabel,
-                'ribbon_bg_color' => $ribbonColor,
-                'ribbon_text_color' => $ribbonTextColor,
-                'more_info_text' => $this->getPromoConfigs()
-            ];
-
-            if ($isLogin && ($availability == '1' || is_array($availability))) {
-                $markIsFeatured = $promotion['field_post_mark_as_featured'][0]['value'];
-
-                if ($promotion['field_casino_gold_only'][0]['value'] && !$isProvisioned) {
-                    continue;
-                }
-
-                $promoProperties = $this->getPostLoginPromotions($promoProperties, $promotion);
-            } else {
-                $markIsFeatured = $promotion['field_mark_as_featured'][0]['value'];
-                $promoProperties = $this->getPreLoginPromotions($promoProperties, $promotion);
-            }
-            if ($markIsFeatured) {
-                $promoPerProduct['featured'][] = $promoProperties + ['category' => 'featured'];
-            }
-
-            $promoPerProduct[$filterId][] = $promoProperties + ['category' => $filterId];
-        }
-
-        $data = $promoPerProduct;
+        $featured = $this->getPromotions($this->getFeatured(), 'featured');
+        $data[] = $featured + $this->getPromotions($promotions);
 
         return $this->rest->output($response, $data);
     }
@@ -153,8 +110,6 @@ class PromotionsComponentController
             'summary_url_title' => $promotion['field_summary_url'][0]['title'] ?? ['title' => ''],
             'summary_blurb' => $promotion['field_summary_blurb'][0]['value'] ?? '',
             'hide_countdown' => $promotion['field_hide_countdown'][0]['value'] ?? true,
-            'hide_promotion' => $promotion['field_hide_promotion'][0]['value'] ?? true,
-            'is_featured' => $promotion['field_mark_as_featured'][0]['value'] ?? false
         ];
     }
 
@@ -168,8 +123,61 @@ class PromotionsComponentController
             'summary_url_target'=> $promotion['field_post_summary_url_target'][0]['value'] ?? '',
             'summary_blurb' => $promotion['field_post_summary_blurb'][0]['value'] ?? '',
             'hide_countdown' => $promotion['field_post_hide_countdown'][0]['value'] ?? true,
-            'hide_promotion' => $promotion['field_post_hide_promotion'][0]['value'] ?? true,
-            'is_featured' => $promotion['field_post_mark_as_featured'][0]['value'] ?? false
         ];
+    }
+
+    private function getPromotions($promotions, $category = NULL)
+    {
+        $promoPerProduct = [];
+        $isLogin = $this->playerSession->isLogin();
+        $isProvisioned = $this->isPlayerProvisioned();
+
+        foreach ($promotions as $promotion) {
+            $filterId = $category ?? $promotion['field_product_category'][0]['field_product_filter_id'][0]['value'];
+            $availability = count($promotion['field_promo_availability']) > 1 ?
+                $promotion['field_promo_availability'] :
+                $promotion['field_promo_availability'][0]['value'];
+
+            $ribbonEnable = $promotion['field_enable_disable_ribbon_tag'][0]['value'] ?? '';
+            $ribbonLabel = $promotion['field_ribbon_label'][0]['value'] ?? '';
+            $ribbonColor = $promotion['field_ribbon_background_color'][0]['color'] ?? '';
+            $ribbonTextColor = $promotion['field_ribbon_text_color'][0]['color'] ?? '';
+
+            $promoProperties = [
+                'title' => $promotion['title'][0]['value'],
+                'product' => $filterId,
+                'ribbon_enable' => $ribbonEnable,
+                'ribbon_label' =>  $ribbonLabel,
+                'ribbon_bg_color' => $ribbonColor,
+                'ribbon_text_color' => $ribbonTextColor,
+                'more_info_text' => $this->getPromoConfigs()
+            ];
+
+            if ($isLogin && ($availability == '1' || is_array($availability))) {
+                $isCasinoOnly = $promotion['field_casino_gold_only'][0]['value'] ?? false;
+                if ($isCasinoOnly && !$isProvisioned) {
+                    continue;
+                }
+
+                $promoProperties = $this->getPostLoginPromotions($promoProperties, $promotion);
+            } else {
+                $promoProperties = $this->getPreLoginPromotions($promoProperties, $promotion);
+            }
+
+            $promoPerProduct[$filterId][] = $promoProperties + ['category' => $filterId];
+        }
+
+        return $promoPerProduct;
+    }
+
+    private function getFeatured()
+    {
+        try {
+            $featured = $this->views->getViewById('featured_promotions');
+        } catch (\Exception $e) {
+            $featured = [];
+        }
+
+        return $featured;
     }
 }
