19467d0ec9c10caaff7e40e5cc1f1d72c45c226c
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-05-24 14:48:13 +0800

    Update marketing script

--- src/Component/Marketing/script.ts
+++ src/Component/Marketing/script.ts
@@ -1,11 +1,15 @@
 import {ComponentInterface, ComponentManager} from "@plugins/ComponentWidget/asset/component";
 
+import {Router, RouterClass} from "@plugins/ComponentWidget/asset/router";
+
 /**
  *
  */
 export class MarketingComponent implements ComponentInterface {
     onLoad(element: HTMLElement, attachments: {}) {
-        // Do nothing
+        Router.on(RouterClass.afterNavigate, (event) => {
+            ComponentManager.refreshComponent("marketing");
+        });
     }
 
     onReload(element: HTMLElement, attachments: {}) {
