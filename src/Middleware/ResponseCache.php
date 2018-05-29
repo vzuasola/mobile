13d0a590b7b9650143437919fc787e7505d21e9e
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-05-29 14:17:57 +0800

    Added override for page cache

---
+++ src/Middleware/ResponseCache.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\MobileEntry\Middleware;
+
+use Interop\Container\ContainerInterface;
+
+use App\Middleware\Cache\ResponseCache as Base;
+
+class ResponseCache extends Base
+{
+    /**
+     * Public constructor
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct($container);
+
+        $this->request = $container->get('request');
+    }
+
+    /**
+     * Gets the cache key
+     */
+    protected function getCacheKey()
+    {
+        $uri = $this->request->getUri();
+
+        $base = $uri->getBaseUrl();
+        $path = $uri->getPath();
+
+        $widget = $this->request->getQueryParam('component-data-widget');
+        $url = trim($uri . $path, '/') . "?component-data-widget=$widget";
+
+        return md5($url);
+    }
+}
