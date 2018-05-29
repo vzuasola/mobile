f00c461dc6219b74142bbdae14843f72abda0875
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-05-29 11:01:09 +0800

    Remove force refresh

--- src/Component/Header/Login/scripts/login.ts
+++ src/Component/Header/Login/scripts/login.ts
@@ -170,9 +170,6 @@ export class Login {
 
                                 this.loader.hide();
                             },
-                            null,
-                            null,
-                            true,
                         );
                     }
                 }
@@ -319,9 +316,6 @@ export class Login {
                     () => {
                         this.loader.hide();
                     },
-                    null,
-                    null,
-                    true,
                 );
             });
         });
