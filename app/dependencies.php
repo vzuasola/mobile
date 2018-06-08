fc4dac06a1b201c67e1f354f9bc3434e932ad088
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2018-06-08 11:31:33 +0800

    Manifest fixes. Header cache fixes.

--- app/dependencies.php
+++ app/dependencies.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\MobileEntry\Dependencies\Manifest;
+
 use App\MobileEntry\Services\CookieService\CookieService;
 use App\MobileEntry\Services\Domains\IDDomain;
 use App\MobileEntry\Services\Accounts\Accounts;
 
 require APP_ROOT . '/core/app/dependencies.php';
+
+$container['asset_manifest'] = function ($c) {
+    return Manifest::create($c);
+};
 
 $container['cookie_service'] = function ($c) {
     return CookieService::create($c);
@@ -17,3 +23,4 @@ $container['id_domain'] = function ($c) {
 $container['accounts_service'] = function ($c) {
     return Accounts::create($c);
 };
+
