b01a6ee2f6e65c2c0de5acef3e3466b2a545c250
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2018-06-21 15:20:32 +0800

    MEPG2-314 Add new loader design on first load

--- assets/script/loader.ts
+++ assets/script/loader.ts
@@ -1,22 +1,20 @@
 import {Loader} from "@app/assets/script/components/loader";
-// import {Splashloader} from "@app/assets/script/components/splash-loader";
+import {Splashloader} from "@app/assets/script/components/splash-loader";
 import {Router, RouterClass} from "@plugins/ComponentWidget/asset/router";
 import {ComponentManager} from "@core/src/Plugins/ComponentWidget/asset/component";
 
 const loader = new Loader(document.body, true);
-// const splashLoader = new Splashloader(document.body, true);
+const splashLoader = new Splashloader(document.body, true);
 
 document.body.setAttribute("style", "");
-loader.show();
-// splashLoader.show();
+splashLoader.show();
 
 ComponentManager.subscribe("components.finish", () => {
-    loader.hide();
+    splashLoader.hide();
 });
 
 Router.on(RouterClass.beforeNavigate, (event) => {
     loader.show();
-    // splashLoader.hide();
 });
 
 Router.on(RouterClass.afterNavigate, (event) => {
