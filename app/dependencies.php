51d2ac8981a1e5cf5b5327d8f490169c2264508f
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-08-03 08:44:37 +0800

    Added Product resolver in container

--- app/dependencies.php
+++ app/dependencies.php
@@ -5,6 +5,7 @@ use App\MobileEntry\Dependencies\Manifest;
 use App\MobileEntry\Services\CookieService\CookieService;
 use App\MobileEntry\Services\Domains\IDDomain;
 use App\MobileEntry\Services\Accounts\Accounts;
+use App\MobileEntry\Services\Product\ProductResolver;
 
 require APP_ROOT . '/core/app/dependencies.php';
 
@@ -23,3 +24,8 @@ $container['id_domain'] = function ($c) {
 $container['accounts_service'] = function ($c) {
     return Accounts::create($c);
 };
+
+$container['product_resolver'] = function ($c) {
+    return ProductResolver::create($c);
+};
+
