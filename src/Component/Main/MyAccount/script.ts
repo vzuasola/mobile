24c03df884c70328365f154fba32fc717fb95249
Leandro Vicario <leandro.vicario@bayviewtechnology.com>
2018-08-03 14:37:53 +0800

    DMA-560 style for checkbox

--- src/Component/Main/MyAccount/script.ts
+++ src/Component/Main/MyAccount/script.ts
@@ -1,6 +1,7 @@
 import * as utility from "@core/assets/js/components/utility";
 import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
 import Tab from "@app/assets/script/components/tab";
+import {CheckboxStyler} from "@app/assets/script/components/checkbox-styler";
 
 /**
  *
@@ -8,13 +9,20 @@ import Tab from "@app/assets/script/components/tab";
 export class MyAccountComponent implements ComponentInterface {
     onLoad(element: HTMLElement, attachments: {}) {
         this.init(element);
+        this.activateCheckboxStyler(element);
     }
 
     onReload(element: HTMLElement, attachments: {}) {
         this.init(element);
+        this.activateCheckboxStyler(element);
     }
 
     private init(element) {
         new Tab();
+    }
+
+    private activateCheckboxStyler(element) {
+        const checkbox = new CheckboxStyler(element.querySelector("#ProfileForm_contact_preference"));
+        checkbox.init();
     }
 }
