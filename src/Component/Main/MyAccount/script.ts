3bc6c700fa0e13b74dd4cae98c3ffca7e6a16dc4
Leandro Vicario <leandro.vicario@bayviewtechnology.com>
2018-08-03 14:55:00 +0800

    DMA-560 code clean up

--- src/Component/Main/MyAccount/script.ts
+++ src/Component/Main/MyAccount/script.ts
@@ -9,19 +9,16 @@ import {CheckboxStyler} from "@app/assets/script/components/checkbox-styler";
 export class MyAccountComponent implements ComponentInterface {
     onLoad(element: HTMLElement, attachments: {}) {
         this.init(element);
-        this.activateCheckboxStyler(element);
     }
 
     onReload(element: HTMLElement, attachments: {}) {
         this.init(element);
-        this.activateCheckboxStyler(element);
     }
 
     private init(element) {
         new Tab();
-    }
 
-    private activateCheckboxStyler(element) {
+        // Checkbox styler
         const checkbox = new CheckboxStyler(element.querySelector("#ProfileForm_contact_preference"));
         checkbox.init();
     }
