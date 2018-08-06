d6d5b941034cddbc2b184eb3acdc98c069c94b03
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2018-08-06 15:54:07 +0800

    dynamic login via in login header

--- src/Component/Header/HeaderComponent.php
+++ src/Component/Header/HeaderComponent.php
@@ -16,8 +16,18 @@ class HeaderComponent implements ComponentWidgetInterface
      */
     private $playerSession;
 
+     /**
+     * @var App\Fetcher\Drupal\ViewsFetcher
+     */
+    private $views;
+
 
     private $menu;
+
+    const PRODUCT_MAPPING = [
+        'mobile-entrypage' => 0,
+        'games' => 'games'
+    ];
 
     /**
      *
@@ -27,18 +37,20 @@ class HeaderComponent implements ComponentWidgetInterface
         return new static(
             $container->get('config_fetcher'),
             $container->get('player_session'),
-            $container->get('menu_fetcher')
+            $container->get('menu_fetcher'),
+            $container->get('views_fetcher')
         );
     }
 
     /**
      * Public constructor
      */
-    public function __construct($configs, $playerSession, $menu)
+    public function __construct($configs, $playerSession, $menu, $views)
     {
         $this->configs = $configs;
         $this->playerSession = $playerSession;
         $this->menu = $menu;
+        $this->views = $views;
     }
 
     /**
@@ -75,6 +87,7 @@ class HeaderComponent implements ComponentWidgetInterface
         $data['mobile_remember'] = $headerConfigs['mobile_remember'] ?? 'Remember Username';
         $data['mobile_login_reg'] = $headerConfigs['mobile_login_reg'] ?? 'Login/Join';
         $data['join_now_link'] = $headerConfigs['join_now_link'] ?? [];
+        $data['product'] = $this->getProduct('games');
 
         try {
             $isLogin = $this->playerSession->isLogin();
@@ -92,5 +105,20 @@ class HeaderComponent implements ComponentWidgetInterface
         }
 
         return $data;
+    }
+
+    private function getProduct($productParam)
+    {
+        $result = [];
+        $products = $this->views->getViewById('products');
+
+        foreach ($products as $key => $product) {
+            if (array_key_exists($product['field_product_instance_id'][0]['value'], $this::PRODUCT_MAPPING)
+                 && $this::PRODUCT_MAPPING[$product['field_product_instance_id'][0]['value']] === $productParam) {
+                return $product;
+            }
+        }
+
+        return [];
     }
 }
