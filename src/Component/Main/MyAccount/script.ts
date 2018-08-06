fb4b568103dcaa1f604f355e2ce3040de2ff71a9
Leandro Vicario <leandro.vicario@bayviewtechnology.com>
2018-08-06 10:26:34 +0800

    DMA-560 script for styling radio inputs

--- src/Component/Main/MyAccount/script.ts
+++ src/Component/Main/MyAccount/script.ts
@@ -2,6 +2,7 @@ import * as utility from "@core/assets/js/components/utility";
 import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
 import Tab from "@app/assets/script/components/tab";
 import {CheckboxStyler} from "@app/assets/script/components/checkbox-styler";
+import {RadioStyler} from "@app/assets/script/components/radio-styler";
 
 /**
  *
@@ -21,5 +22,8 @@ export class MyAccountComponent implements ComponentInterface {
         // Checkbox styler
         const checkbox = new CheckboxStyler(element.querySelector("#ProfileForm_contact_preference"));
         checkbox.init();
+
+        const radio = new RadioStyler("#MyProfileForm_gender");
+        radio.init();
     }
 }
