342172d056d33bbd45f1499e9f49db4cfb5a3a95
rjose <regie.jose@bayviewtechnology.com>
2018-08-17 13:58:59 +0800

    Adding my profile ajax request

--- src/Component/Main/MyAccount/MyAccountComponentController.php
+++ src/Component/Main/MyAccount/MyAccountComponentController.php
@@ -18,23 +18,37 @@ class MyAccountComponentController
     private $changePassword;
 
     /**
+     * User Fetcher Object
+     */
+    private $userFetcher;
+
+    /**
+     * Receive News Object
+     */
+    private $receiveNews;
+
+    /**
      *
      */
     public static function create($container)
     {
         return new static(
             $container->get('rest'),
-            $container->get('change_password')
+            $container->get('change_password'),
+            $container->get('user_fetcher'),
+            $container->get('receive_news')
         );
     }
 
     /**
      * Public constructor
      */
-    public function __construct($rest, $changePassword)
+    public function __construct($rest, $changePassword, $userFetcher, $receiveNews)
     {
         $this->rest = $rest;
         $this->changePassword = $changePassword;
+        $this->userFetcher = $userFetcher;
+        $this->receiveNews = $receiveNews;
     }
 
     /**
@@ -60,6 +74,58 @@ class MyAccountComponentController
 
         return $this->rest->output($response, [
             'status' => $status,
+        ]);
+    }
+
+    /**
+     * Ajax - update profile request
+     */
+    public function updateprofile($request, $response)
+    {
+        $defaultValues = $this->userFetcher->getPlayerDetails();
+        $profileFormValues = $request->getParsedBody();
+        $receiveNews = false;
+        $mobile1 = null;
+
+        $mobileNumber = $defaultValues['mobileNumbers']['Home']['number'] ?? "";
+        $mobileNumberVerified = $defaultValues['mobileNumbers']['Home']['verified'] ?? false;
+
+        if (isset($profileFormValues['mobile']) && !$mobileNumberVerified) {
+            $mobileNumber = $profileFormValues['mobile'];
+        }
+
+        if (isset($profileFormValues['mobile1']) ?? $profileFormValues['mobile1']) {
+            $mobile1 = $profileFormValues['mobile1'];
+        }
+
+        if (isset($profileFormValues['receive_news'])) {
+            $receiveNews = true;
+        }
+
+        $playerDetails = [
+            'username' => $defaultValues['username'],
+            'firstname' => $defaultValues['firstName'],
+            'lastname' => $defaultValues['lastName'],
+            'email' => $defaultValues['email'],
+            'countryid' => $defaultValues['countryId'],
+            'gender' => $profileFormValues['gender'],
+            'language' => $profileFormValues['language'],
+            'mobile' => $mobileNumber,
+            'mobile1' => $mobile1,
+            'address' => $profileFormValues['address'],
+            'city' => $profileFormValues['city'],
+            'postalcode' => $profileFormValues['postal_code'],
+        ];
+
+        try {
+            $status = $this->userFetcher->setPlayerDetails($playerDetails);
+            $this->receiveNews->setSubscription($receiveNews);
+        } catch (\Exception $e) {
+            return $response->withStatus(500);
+        }
+
+        return $this->rest->output($response, [
+            'status' => 'UPDATE_PROFILE_SUCCESS'
         ]);
     }
 }
