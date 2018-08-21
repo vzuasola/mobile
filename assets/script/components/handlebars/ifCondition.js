985ff21b3d8e2b60edf092c39977f4e6cec04c5a
Leandro Vicario <leandro.vicario@bayviewtechnology.com>
2018-08-21 14:48:53 +0800

    MDMA-16 remove none strict condition

--- assets/script/components/handlebars/ifCondition.js
+++ assets/script/components/handlebars/ifCondition.js
@@ -3,12 +3,8 @@
  */
 module.exports = function (v1, operator, v2, options) {
     switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
         case '===':
             return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '!=':
-            return (v1 != v2) ? options.fn(this) : options.inverse(this);
         case '!==':
             return (v1 !== v2) ? options.fn(this) : options.inverse(this);
         case '<':
