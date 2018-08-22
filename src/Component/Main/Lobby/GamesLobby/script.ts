ac1ba3d2dac1cce57c80e1c085ccbeb00cbbb9b1
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-08-22 09:44:36 +0800

    Deeplink support

--- src/Component/Main/Lobby/GamesLobby/script.ts
+++ src/Component/Main/Lobby/GamesLobby/script.ts
@@ -15,6 +15,7 @@ import {Loader} from "@app/assets/script/components/loader";
  */
 export class GamesLobbyComponent implements ComponentInterface {
     private element: HTMLElement;
+    private response: any;
 
     onLoad(element: HTMLElement, attachments: {authenticated: boolean}) {
         this.element = element;
@@ -26,19 +27,48 @@ export class GamesLobbyComponent implements ComponentInterface {
         this.generateLobby();
     }
 
+    /**
+     * Initialized games lobby
+     */
     private generateLobby() {
+
+        if (!this.response) {
+            this.doRequest();
+        } else {
+            this.setLobby();
+        }
+    }
+
+    /**
+     * Request games lobby to games lobby component controller lobby method
+     */
+    private doRequest() {
         xhr({
             url: Router.generateRoute("games_lobby", "lobby"),
             type: "json",
         }).then((response) => {
-            const key = response.categories[0].field_games_alias;
-            this.setCategories(response.categories);
-            this.setGames(response.games[key]);
+            this.response = response;
+            this.setLobby();
         }).fail((error, message) => {
             console.log(error);
         });
     }
 
+    /**
+     * Populate lobby with the set response
+     */
+    private setLobby() {
+        let key = this.response.categories[0].field_games_alias;
+        key = this.getActiveCategory(this.response.games, key);
+
+        this.setCategories(this.response.categories);
+        this.setGames(this.response.games[key]);
+    }
+
+    /**
+     * Set the category in the template
+     *
+     */
     private setCategories(data) {
         const template = categoriesTemplate({
             categories: data,
@@ -51,6 +81,23 @@ export class GamesLobbyComponent implements ComponentInterface {
         }
     }
 
+    /**
+     * Get the hash in the url or the first returned category
+     *
+     */
+    private getActiveCategory(gamesList, key) {
+        const hash = utility.getHash(window.location.href);
+
+        if (gamesList[hash]) {
+            return hash;
+        }
+
+        return key;
+    }
+
+    /**
+     * Set the games list in the template
+     */
     private setGames(data) {
         const template = gameTemplate({
             games: data,
