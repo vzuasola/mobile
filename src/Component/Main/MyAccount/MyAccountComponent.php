f2850fa9c265f0a4340aa74371a3a8b0c761fd2e
Rainer Cedric Mariano <RainerCedric.Mariano@bayviewtechnology.com>
2018-08-24 13:46:33 +0800

    add configurable name tabs and fix twig variable names

--- src/Component/Main/MyAccount/MyAccountComponent.php
+++ src/Component/Main/MyAccount/MyAccountComponent.php
@@ -9,6 +9,31 @@ use App\MobileEntry\Form\ResetPasswordForm;
 
 class MyAccountComponent implements ComponentWidgetInterface
 {
+        /**
+     * Config Fetcher object.
+     */
+    private $configFetcher;
+
+    /**
+     *
+     */
+    public static function create($container)
+    {
+        return new static(
+            $container->get('form_manager'),
+            $container->get('config_fetcher')
+        );
+    }
+
+    /**
+     * Public constructor
+     */
+    public function __construct($formManager, $configFetcher)
+    {
+        $this->formManager = $formManager;
+        $this->configFetcher = $configFetcher->withProduct('account');
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -22,6 +47,11 @@ class MyAccountComponent implements ComponentWidgetInterface
      */
     public function getData()
     {
-        return [];
+        $myProfileConfig = $this->configFetcher->getConfigById('my_account_profile_general_configuration');
+
+        return [
+            'myProfileTab' => $myProfileConfig['my_profile_tab'] ?? 'My Profile',
+            'changePasswordTab' => $myProfileConfig['change_password_tab'] ?? 'Change Password',
+        ];
     }
 }
