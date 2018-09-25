db2948a57907e81003021a443781c4f58195ac0a
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2018-09-25 15:09:26 +0800

    remove whitespace

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -15,7 +15,6 @@ class GamesController extends BaseController
             $config = $this->get('config_fetcher')
             ->withProduct($this->get('product_resolver')->getProduct())
             ->getConfig('webcomposer_config.header_configuration');
-
         } catch (\Exception $e) {
             $config = [];
         }
