35e6e6e30fe742a59a63ba11a283566676769976
Daniel Tracy Dela Cruz <danieltracy.delacruz@bayviewtechnology.com>
2018-12-04 14:05:31 +0800

    Alias support

--- src/Services/Product/Products.php
+++ src/Services/Product/Products.php
@@ -10,4 +10,86 @@ class Products
     const PRODUCT_MAPPING = [
         'games' => 'mobile-games'
     ];
+
+    const PRODUCT_ALIAS = [
+        'casino' => [
+            'casino',
+            'dafa888',
+            '888',
+            'yulechang',
+            'xuanzhuan',
+            'duchang',
+            'casino-gold',
+            'gold',
+        ],
+        'sports' => [
+            'sports',
+            'football',
+            'soccer',
+            'goal',
+            'cup',
+            'match168',
+        ],
+        'sports-df' => [
+            'sports-df',
+            'dffootball',
+            'dfsoccer',
+            'dfgoal',
+            'dfcup',
+            'dfmatch168',
+        ],
+        'lottery' => [
+            'keno',
+            'lottery',
+            'lotto-numbers',
+            'numbers',
+            'draw',
+            'live-draws',
+        ],
+        'live-dealer' => [
+            'live-dealer',
+            'macau',
+            'live',
+            'live-games',
+            'xianchang',
+            'zhenren',
+        ],
+        'games' => [
+            'games',
+            'slots',
+            'flashgames',
+            'laohuji',
+            'playflash',
+            'spinwin',
+        ],
+        'exchange' => [
+            'exchange',
+            'cricket',
+            'indiabet',
+            'cric-exchange',
+            'betting-exchange',
+        ],
+        'arcade' => [
+            'arcade',
+            'youxi',
+            'xiaoyouxi',
+            'fun',
+            'amusement',
+            'arcade-room',
+            'fish-hunter',
+            'fish',
+            'fishing',
+            'buyu',
+            'buyuwang',
+            'buyudaren',
+        ],
+        'virtual-sports' => [
+            'virtual-sports',
+            'virtual-world',
+            'virtualsports',
+            'virtual-network',
+            'virtual-sports-coupon',
+            'virtuals',
+        ],
+    ];
 }
