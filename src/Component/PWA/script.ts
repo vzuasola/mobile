ad3d6517f844ef690543d20da139029cdb2da775
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2019-01-16 14:47:28 +0800

    Update component for pwa

--- src/Component/PWA/script.ts
+++ src/Component/PWA/script.ts
@@ -11,10 +11,33 @@ export class PWAComponent implements ComponentInterface {
     private originalUrl: string;
 
     onLoad(element: HTMLElement, attachments: {}) {
-        // Do nothing
+        this.element = element;
+        this.showPWA();
+        this.listenOnPWAClose();
     }
 
     onReload(element: HTMLElement, attachments: {}) {
-        // Do nothing
+        this.showPWA();
+    }
+
+    private showPWA() {
+        setTimeout(() => {
+            this.togglePWA();
+        }, 1 * 1000);
+    }
+
+    private listenOnPWAClose() {
+        ComponentManager.subscribe("click", (event, src) => {
+            if (utility.hasClass(src, "btn-pwa-close", 1)) {
+                this.togglePWA();
+            }
+        });
+    }
+
+    private togglePWA() {
+        const pwa = this.element.querySelector(".btn-pwa");
+        if (pwa) {
+            utility.toggleClass(pwa, "hidden");
+        }
     }
 }
