78e7bebd68015af09c7823b3e9cc3b05d469177c
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2019-01-28 15:30:29 +0800

    Add new cache for paths

--- assets/script/worker.ts
+++ assets/script/worker.ts
@@ -38,6 +38,22 @@ if (workbox) {
             ],
         }),
     );
+
+    workbox.routing.registerRoute(
+        // Cache image files
+        new RegExp("/.*"),
+        // Use the cache if it's available
+        workbox.strategies.networkFirst({
+            // Use a custom cache name
+            cacheName: "page-cache",
+            plugins: [
+                new workbox.expiration.Plugin({
+                    // Cache for a maximum of a week
+                    maxAgeSeconds: 7 * 24 * 60 * 60,
+                }),
+            ],
+        }),
+    );
 }
 
 const cacheKey = "dafabet-mobile-v1.0";
