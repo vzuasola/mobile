3afb0d8b760763fb5c4a597720d0aac83455d400
Alexander Nikko Tenepere <alexandernikko.tenepere@bayviewtechnology.com>
2019-01-29 15:06:24 +0800

    Added conditional manifest

---
+++ assets/script/worker-registration.ts
@@ -0,0 +1,21 @@
+import * as Bowser from "bowser";
+
+const browser = Bowser.getParser(window.navigator.userAgent);
+
+const isSupported = browser.satisfies({
+    chrome: ">=20",
+    safari: ">=9",
+});
+
+if (isSupported) {
+    // Service worker registraton
+    if ("serviceWorker" in window.navigator) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker.register("/sw.js");
+        });
+    }
+} else {
+    const manifest = document.querySelector("link[rel='manifest']");
+
+    manifest.remove();
+}
