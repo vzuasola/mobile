b150d4b0373c67f8fe2c61223a7871310bfedefd
Leandro Vicario <leandro.vicario@bayviewtechnology.com>
2019-02-11 07:33:25 +0800

    MDMA-103 remove console log

--- src/Component/Main/MyAccount/Profile/scripts/profile.ts
+++ src/Component/Main/MyAccount/Profile/scripts/profile.ts
@@ -65,7 +65,6 @@ export class Profile extends FormBase {
 
     private getUserData() {
         const user = this.attachments.user;
-        console.log("user", user);
 
         return {
             firstname: user.first_name,
