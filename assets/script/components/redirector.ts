5d6ed992a0bce28936ff596e882a7f18a34d7b7f
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2019-04-01 09:38:01 +0800

    Update the redirect for pwa to window location

--- assets/script/components/redirector.ts
+++ assets/script/components/redirector.ts
@@ -13,13 +13,8 @@ class Redirector {
      * @param closure A optional handler callback
      */
     redirect(url: string, closure?) {
-        // Safari
-        const iOSSafari = /iP(ad|od|hone)/i.test(window.navigator.userAgent) &&
-        /WebKit/i.test(window.navigator.userAgent) &&
-        !(/(CriOS|FxiOS|OPiOS|mercury)/i.test(window.navigator.userAgent));
-
         // handle redirects if we are on a PWA standalone
-        if (!iOSSafari && (navigator.standalone || window.matchMedia("(display-mode: standalone)").matches)) {
+        if ((navigator.standalone || window.matchMedia("(display-mode: standalone)").matches)) {
             url = utility.addQueryParam(url, "source", "pwa");
 
             // Check if re is in the parameters and add source=pwa to re param
@@ -29,14 +24,6 @@ class Redirector {
                 reUrl = utility.addQueryParam(reUrl, "source", "pwa");
                 url = utility.addQueryParam(url, "re", reUrl);
             }
-
-            window.open(url);
-
-            if (closure) {
-                closure();
-            }
-
-            return;
         }
 
         window.location.href = url;
