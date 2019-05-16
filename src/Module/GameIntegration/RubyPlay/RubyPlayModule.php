196621873311fcc884fe4d0bae33a572c2a3e400
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2019-05-16 10:20:44 +0800

    ruby play integration

---
+++ src/Module/GameIntegration/RubyPlay/RubyPlayModule.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\MobileEntry\Module\GameIntegration\RubyPlay;
+
+use App\Plugins\ComponentWidget\ComponentModuleInterface;
+
+class RubyPlayModule implements ComponentModuleInterface
+{
+}
