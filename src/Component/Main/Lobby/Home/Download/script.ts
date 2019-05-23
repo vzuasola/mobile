5163f2e7a9718e093434f7ffa3e066908d28fd87
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2019-05-23 16:29:19 +0800

    Download Section Collapsible tiles

--- src/Component/Main/Lobby/Home/Download/script.ts
+++ src/Component/Main/Lobby/Home/Download/script.ts
@@ -2,6 +2,7 @@ import * as utility from "@core/assets/js/components/utility";
 
 import EqualHeight from "@app/assets/script/components/equal-height";
 import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
+import Accordion from "@app/assets/script/components/accordion";
 
 /**
  *
@@ -10,11 +11,13 @@ export class DownloadComponent implements ComponentInterface {
     onLoad(element: HTMLElement, attachments: {}) {
         this.downloadsVisibility(element);
         this.equalizeDownloadHeight();
+        this.accordion(element);
     }
 
     onReload(element: HTMLElement, attachments: {}) {
         this.downloadsVisibility(element);
         this.equalizeDownloadHeight();
+        this.accordion(element);
     }
 
     private downloadsVisibility(element) {
@@ -28,5 +31,9 @@ export class DownloadComponent implements ComponentInterface {
     private equalizeDownloadHeight() {
         const equalDownload = new EqualHeight(".download-box");
         equalDownload.init();
+    }
+
+    private accordion(element) {
+        const accordion = new Accordion(element);
     }
 }
