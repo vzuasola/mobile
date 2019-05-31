aad760c3fd800429ee9f5ce300cec2f9deae1c37
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2019-05-31 13:24:40 +0800

    allbet integration

---
+++ src/Module/GameIntegration/AllBet/AllBetModule.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\MobileEntry\Module\GameIntegration\AllBet;
+
+use App\Plugins\ComponentWidget\ComponentModuleInterface;
+
+class AllBetModule implements ComponentModuleInterface
+{
+}
