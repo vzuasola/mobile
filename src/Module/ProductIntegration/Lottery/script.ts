16e2f1a7c9a1020c5413dd36fa6e6b75396c500e
anarvaza <AllenKenneth.Narvaza@bayviewtechnology.com>
2019-05-31 14:28:02 +0800

    Fix Menu list issues

---
+++ src/Module/ProductIntegration/Lottery/script.ts
@@ -0,0 +1,53 @@
+import * as utility from "@core/assets/js/components/utility";
+import * as xhr from "@core/assets/js/vendor/reqwest";
+
+import {ComponentManager, ModuleInterface} from "@plugins/ComponentWidget/asset/component";
+import {Router} from "@plugins/ComponentWidget/asset/router";
+import {Redirector} from "@app/assets/script/components/redirector";
+
+import {Redirectable} from "../scripts/redirectable";
+
+export class LiveDealerIntegrationModule extends Redirectable implements ModuleInterface {
+    protected code = "lottery";
+    protected isLoginOnly = false;
+
+    protected doRequest(src) {
+        const url = src.getAttribute("data-post-login-url");
+
+        if (url) {
+            this.loader.show();
+
+            xhr({
+                url: Router.generateModuleRoute("login_redirect", "process"),
+                type: "json",
+                method: "post",
+                data: {
+                    url,
+                },
+            }).then((response) => {
+                if (typeof response.url !== "undefined") {
+                    if (!this.isSupportedLanguage(src)) {
+                        response.url = response.url.replace(
+                            "\/" + ComponentManager.getAttribute("language") + "\/",
+                            "/" + this.getRedirectLanguage(src) + "/");
+                    }
+                    if (utility.isExternal(response.url)) {
+                        Redirector.redirect(response.url, () => {
+                            this.loader.hide();
+                        });
+
+                        return;
+                    }
+
+                    Router.navigate(response.url, ["*"], {removeParams: ["product"]});
+                    this.loader.hide();
+                    return;
+                }
+
+                this.loader.hide();
+            }).fail((error, message) => {
+                this.loader.hide();
+            });
+        }
+    }
+}
