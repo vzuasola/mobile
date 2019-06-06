af77f1989d831ae60a10f8eba84fa470b4a0b282
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2019-06-06 17:42:31 +0800

    Add override for PAS

--- src/Module/GameIntegration/PAS/PASModuleController.php
+++ src/Module/GameIntegration/PAS/PASModuleController.php
@@ -3,6 +3,7 @@
 namespace App\MobileEntry\Module\GameIntegration\PAS;
 
 use App\MobileEntry\Module\GameIntegration\ProviderTrait;
+use App\Drupal\Config;
 
 class PASModuleController
 {
@@ -109,6 +110,7 @@ class PASModuleController
         $requestData = $request->getParsedBody();
         $data['gameurl'] = false;
         $data['currency'] = false;
+
         if ($this->checkCurrency($requestData['currency'])) {
             $data['currency'] = true;
 
@@ -159,4 +161,49 @@ class PASModuleController
 
         return $this->rest->output($response, $data);
     }
+
+    /**
+     * Override trait
+     *
+     */
+    public function unsupported($request, $response)
+    {
+        try {
+            $config =  $this->config->getConfig('webcomposer_config.unsupported_currency');
+            $providerMapping = Config::parse($config['game_provider_mapping'] ?? '');
+            $data['provider'] = $providerMapping[self::KEY];
+            $data['title'] = $config['unsupported_currencies_title'] ?? '';
+            $data['message'] =
+                $config['unsupported_currencies_message']['value'] ?? '';
+            $data['button'] = $config['unsupported_currencies_button'] ?? '';
+            $data['status'] = true;
+        } catch (\Exception $e) {
+            $data['status'] = false;
+        }
+
+        return $this->rest->output($response, $data);
+    }
+
+    /**
+     * Override trait
+     *
+     */
+    private function checkCurrency($playerCurrency)
+    {
+        try {
+            $config =  $this->config->getConfig('webcomposer_config.icore_playtech_provider');
+            $currencies = explode("\r\n", $config['dafabetgames_currency']);
+            if (!$playerCurrency) {
+                $playerCurrency = $this->player->getCurrency();
+            }
+
+            if (in_array($playerCurrency, $currencies)) {
+                return true;
+            }
+        } catch (\Exception $e) {
+            // Do nothing
+        }
+        return false;
+    }
+
 }
