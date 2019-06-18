7f6b45f85944a86f8be7e504b6f11e2662f09155
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2019-06-18 18:05:01 +0800

    entrypage dafaconnect mobile hide view desktop

--- src/Component/Footer/FooterComponentController.php
+++ src/Component/Footer/FooterComponentController.php
@@ -11,15 +11,6 @@ class FooterComponentController
      */
     private $menus;
 
-    /**
-     * @var App\Fetcher\Drupal\ViewsFetcher
-     */
-    private $views;
-
-    private $idDomain;
-
-    private $product;
-
     private $rest;
 
     /**
@@ -29,9 +20,6 @@ class FooterComponentController
     {
         return new static(
             $container->get('menu_fetcher'),
-            $container->get('views_fetcher'),
-            $container->get('id_domain'),
-            $container->get('product_resolver'),
             $container->get('rest')
         );
     }
@@ -41,15 +29,9 @@ class FooterComponentController
      */
     public function __construct(
         $menus,
-        $views,
-        $idDomain,
-        $product,
         $rest
     ) {
         $this->menus = $menus;
-        $this->views = $views;
-        $this->product = $product;
-        $this->idDomain = $idDomain;
         $this->rest = $rest;
     }
 
@@ -63,61 +45,12 @@ class FooterComponentController
         $data = [];
 
         try {
-            $data['sponsors'] = $this->views->getViewById('mobile_sponsor_list');
-        } catch (\Exception $e) {
-            $data['sponsors'] = [];
-        }
-
-        try {
             $data['footer_menu'] = $this->menus->getMultilingualMenu('mobile-footer');
         } catch (\Exception $e) {
             $data['footer_menu'] = [];
             ddd($e->getMessage());
         }
 
-        $this->cleanFooterMenu($data['footer_menu']);
-        $this->orderSponsors($data['sponsors']);
-
-        $data['copyright'] = 'Copyright';
-
         return $this->rest->output($response, $data);
     }
-
-    private function orderSponsors(&$sponsors)
-    {
-        $count = 1;
-
-        foreach ($sponsors as $key => $sponsor) {
-            if (!$sponsor['field_mobile_full_row'][0]['value']) {
-                if ($count == 1) {
-                    $sponsors[$key]['leaf_class'] = 'left';
-                }
-
-                if ($count == 2) {
-                    $sponsors[$key]['leaf_class'] = 'right';
-                }
-
-                $count++;
-            }
-
-            if ($count > 2) {
-                $count = 1;
-            }
-        }
-    }
-
-    private function cleanFooterMenu(&$footerMenu)
-    {
-        if ($footerMenu) {
-            foreach ($footerMenu as $key => $link) {
-                if (($this->idDomain->isLangSelectorHidden() ||
-                    $this->product->getProduct() == 'mobile-casino-gold') &&
-                    strpos($link['attributes']['class'], 'language-trigger') !== false
-                ) {
-                    unset($footerMenu[$key]);
-                }
-            }
-        }
-    }
-
 }
