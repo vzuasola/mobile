ffaa296994c4543f5422cb148c06a0432d3f2b11
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2019-06-24 11:58:38 +0800

    Revert

--- src/Component/Main/Lobby/GamesLobby/script.ts
+++ src/Component/Main/Lobby/GamesLobby/script.ts
@@ -1081,9 +1081,7 @@ export class GamesLobbyComponent implements ComponentInterface {
             for (const key in data.options) {
                 if (data.options.hasOwnProperty(key)) {
                     const param = data.options[key];
-                    if (key !== "loader") {
-                        url = utility.addQueryParam(url, key, param);
-                    }
+                    url = utility.addQueryParam(url, key, param);
                 }
             }
 
