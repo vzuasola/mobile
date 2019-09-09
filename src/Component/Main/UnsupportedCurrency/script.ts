ee160739a801bebb91eabb51648ce8aa276d9ab5
anarvaza <AllenKenneth.Narvaza@bayviewtechnology.com>
2019-09-09 19:17:10 +0800

    Added files for sonar cube

--- src/Component/Main/UnsupportedCurrency/script.ts
+++ src/Component/Main/UnsupportedCurrency/script.ts
@@ -6,10 +6,12 @@ import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
 export class UnsupportedCurrencyComponent implements ComponentInterface {
 
     onLoad(element: HTMLElement, attachments: {}) {
+        // On load
         // Nothing to do here
     }
 
     onReload(element: HTMLElement, attachments: {}) {
+        // On reload
         // Nothing to do here
     }
 }
