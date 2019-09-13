3c1403292aec1abbd506a36a4f1b9619a8d25929
Ronald Duque <ronald.duque@bayviewtechnology.com>
2019-09-13 17:37:05 +0800

    Added Soda Module

---
+++ src/Module/ProductIntegration/SodaCasino/script.ts
@@ -0,0 +1,46 @@
+import * as utility from "@core/assets/js/components/utility";
+import * as xhr from "@core/assets/js/vendor/reqwest";
+
+import {ComponentManager, ModuleInterface} from "@plugins/ComponentWidget/asset/component";
+import {Router} from "@plugins/ComponentWidget/asset/router";
+import {Redirector} from "@app/assets/script/components/redirector";
+
+import {Redirectable} from "../scripts/redirectable";
+
+export class SodaCasinoIntegrationModule extends Redirectable implements ModuleInterface {
+    protected code = "casino";
+    protected isLoginOnly = false;
+
+    doRequest(src) {
+        this.getPreferredCasino(src);
+    }
+
+    private getPreferredCasino(src) {
+        this.loader.show();
+
+        xhr({
+            url: Router.generateRoute("casino_option", "preference"),
+            type: "json",
+        }).then((response) => {
+            if (response.success) {
+                if (!this.isSupportedLanguage(src)) {
+                    response.redirect = response.redirect.replace(
+                        "\/" + ComponentManager.getAttribute("language") + "\/",
+                        "/" + this.getRedirectLanguage(src) + "/");
+                }
+                // redirect to URL
+                if (response.redirect) {
+                    Router.navigate(response.redirect, ["*"], {removeParams: ["product"]});
+                    this.loader.hide();
+                    return;
+                }
+
+                ComponentManager.broadcast("casino.preference");
+            }
+
+            this.loader.hide();
+        }).fail((error, message) => {
+            this.loader.hide();
+        });
+    }
+}
