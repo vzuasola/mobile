c4c73562022652ca5588859250f2e86f5abafb76
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2019-12-09 18:15:01 +0800

    Updated contact us page fetch data

--- src/Component/Main/ContactUsForm/script.ts
+++ src/Component/Main/ContactUsForm/script.ts
@@ -1,13 +1,15 @@
 import * as utility from "@core/assets/js/components/utility";
-import {ComponentInterface, ComponentManager} from "@plugins/ComponentWidget/asset/component";
-import Tab from "@app/assets/script/components/tab";
-import {Marker} from "@app/assets/script/components/marker";
-import * as iconCheckedTemplate from "@app/templates/handlebars/icon-checked.handlebars";
-import * as iconUnCheckedTemplate from "@app/templates/handlebars/icon-unchecked.handlebars";
-import {Router, RouterClass} from "@plugins/ComponentWidget/asset/router";
-import EqualHeight from "@app/assets/script/components/equal-height";
+import * as Handlebars from "handlebars/runtime";
+
+import * as xhr from "@core/assets/js/vendor/reqwest";
+
+import * as contactUsTabsTemplate from "./handlebars/contact-us-tabs.handlebars";
+import { Router } from "@plugins/ComponentWidget/asset/router";
+
 import {annotation} from "@app/assets/script/components/form-annotation";
 import {ContactUsForm} from "./scripts/contact-us";
+
+import {ComponentInterface, ComponentManager} from "@plugins/ComponentWidget/asset/component";
 
 /**
  *
@@ -15,17 +17,20 @@ import {ContactUsForm} from "./scripts/contact-us";
 export class ContactUsFormComponent implements ComponentInterface {
     private element: HTMLElement;
     private contactus: any;
+    private contactUsTabsData: any;
 
     onLoad(element: HTMLElement, attachments: {}) {
+        this.element = element;
         this.activateFormAnnotation(element);
         this.activateContactUs(element, attachments);
-        this.countrySelection();
+        this.getContactUsTabs();
     }
 
     onReload(element: HTMLElement, attachments: {}) {
+        this.element = element;
         this.activateFormAnnotation(element);
         this.activateContactUs(element, attachments);
-        this.countrySelection();
+        this.getContactUsTabs();
     }
 
     private activateFormAnnotation(element) {
@@ -82,4 +87,30 @@ export class ContactUsFormComponent implements ComponentInterface {
             });
         }
     }
+
+    private getContactUsTabs() {
+        xhr({
+            url: Router.generateRoute("contact_us", "contactUsTabs"),
+            type: "json",
+        }).then((response) => {
+            this.contactUsTabsData = response;
+            this.generateContactUsTabsMarkup(this.contactUsTabsData);
+            this.countrySelection();
+        });
+    }
+
+    /**
+     * Set the contact us tabs in the template
+     *
+     */
+    private generateContactUsTabsMarkup(data) {
+        const contactUsTabs: HTMLElement = this.element.querySelector("#contact-us-tabs");
+        const template = contactUsTabsTemplate({
+            contactUsTabsData: data,
+            contactBlurbTitle: data.contact_blurb.page_title,
+            contactBlurbContent: data.contact_blurb.body_content.value,
+        });
+        contactUsTabs.innerHTML = template;
+    }
+
 }
