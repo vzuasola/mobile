671b28931eb87855635376cd7f33bef42fc67870
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2020-01-16 19:37:50 +0800

    Fix for iframe

--- src/Middleware/XFrames.php
+++ src/Middleware/XFrames.php
@@ -5,32 +5,21 @@ namespace App\MobileEntry\Middleware;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Interop\Container\ContainerInterface;
+use App\MobileEntry\Services\Product\Products;
 
 use App\Plugins\Middleware\ResponseMiddlewareInterface;
 
 class XFrames implements ResponseMiddlewareInterface
 {
-    const PRODUCTS = [
-        'mobile-soda-casino'
-    ];
-
-    /**
-     * Public constructor
-     *
-     * @param ContainerInterface $container
-     */
-    public function __construct(ContainerInterface $container)
-    {
-        $this->product = $container->get('product_resolver')->getProduct();
-    }
-
     /**
      * {@inheritdoc}
      */
     public function handleResponse(RequestInterface &$request, ResponseInterface &$response)
     {
         try {
-            if (in_array($this->product, self::PRODUCTS)) {
+            $path = $request->getUri()->getPath();
+
+            if (in_array(explode('/', $path)[1], Products::PRODUCT_ALIAS['soda-casino'])) {
                 $response = $response->withHeader('X-Frame-Options', 'Deny');
             }
         } catch (\Exception $e) {
