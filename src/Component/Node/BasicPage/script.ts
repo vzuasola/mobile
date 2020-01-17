86779d2975bb02ec87870b1f1ec5726be95a5bbb
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2020-01-17 14:18:29 +0800

    SODA Maintenance page: initial commit

--- src/Component/Node/BasicPage/script.ts
+++ src/Component/Node/BasicPage/script.ts
@@ -1,14 +1,27 @@
-import {ComponentInterface, ComponentManager} from "@plugins/ComponentWidget/asset/component";
+import * as utility from "@core/assets/js/components/utility";
+
+import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
+import {Loader} from "@app/assets/script/components/loader";
 
 /**
  * Generic Basic Pages
  */
 export class BasicPageComponent implements ComponentInterface {
-    onLoad(element: HTMLElement, attachments: {}) {
-        // Do Nothing
+    private loader: Loader;
+
+    constructor() {
+        this.loader = new Loader(document.body, true);
     }
 
-    onReload(element: HTMLElement, attachments: {}) {
-        // Do Nothing
+    onLoad(element: HTMLElement, attachments: {isMatch: boolean, url: string}) {
+        if (attachments.isMatch === false) {
+            this.loader.show();
+            window.location.replace(attachments.url);
+        }
+    }
+
+    onReload(element: HTMLElement, attachments: {url: string}) {
+        this.loader.show();
+        window.location.replace(attachments.url);
     }
 }
