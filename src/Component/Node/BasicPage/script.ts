d6339919de8b11230cd59ea4620c446923bc83cf
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2020-01-20 10:49:14 +0800

    mobile casino soda added config for maintenance page removed unused file

--- src/Component/Node/BasicPage/script.ts
+++ src/Component/Node/BasicPage/script.ts
@@ -1,27 +1,14 @@
-import * as utility from "@core/assets/js/components/utility";
-
-import {ComponentInterface} from "@plugins/ComponentWidget/asset/component";
-import {Loader} from "@app/assets/script/components/loader";
+import {ComponentInterface, ComponentManager} from "@plugins/ComponentWidget/asset/component";
 
 /**
  * Generic Basic Pages
  */
 export class BasicPageComponent implements ComponentInterface {
-    private loader: Loader;
-
-    constructor() {
-        this.loader = new Loader(document.body, true);
+    onLoad(element: HTMLElement, attachments: {}) {
+        // Do Nothing
     }
 
-    onLoad(element: HTMLElement, attachments: {isMatch: boolean, url: string}) {
-        if (attachments.isMatch === false) {
-            this.loader.show();
-            window.location.replace(attachments.url);
-        }
-    }
-
-    onReload(element: HTMLElement, attachments: {url: string}) {
-        this.loader.show();
-        window.location.replace(attachments.url);
+    onReload(element: HTMLElement, attachments: {}) {
+        // Do Nothing
     }
 }
