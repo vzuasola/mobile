461481fb9018796d584075f3e5811b4a49bef3c4
Vladimir Zuasola <vladimir.zuasola@bayviewtechnology.com>
2020-02-11 15:16:11 +0800

    soft maintenance page soda: initial commit Added maintenance component

--- src/Middleware/SiteMaintenance.php
+++ src/Middleware/SiteMaintenance.php
@@ -16,7 +16,13 @@ use App\Drupal\Config;
 class SiteMaintenance implements ResponseMiddlewareInterface
 {
     const PRODUCTS = [
+        'mobile-games',
+        'mobile-casino',
+        'mobile-casino-gold',
+        'mobile-live-dealer',
+        'mobile-lottery',
         'mobile-exchange',
+        'mobile-arcade',
         'mobile-soda-casino'
     ];
 
@@ -33,6 +39,7 @@ class SiteMaintenance implements ResponseMiddlewareInterface
         $this->playerSession = $container->get('player_session');
         $this->product = $container->get('product_resolver')->getProduct();
         $this->configFetcher = $container->get('config_fetcher')->withProduct($this->product);
+        $this->configs = $container->get('config_fetcher')->getGeneralConfigById('webcomposer_site_maintenance');
 
         // route configuration
         $this->route = $this->router->getRouteConfiguration($this->request);
@@ -45,35 +52,42 @@ class SiteMaintenance implements ResponseMiddlewareInterface
     {
         try {
             if (in_array($this->product, self::PRODUCTS)) {
-                $stream = fopen('php://memory', 'r+');
-                fwrite($stream, '');
-                $response = $response->withBody(new Body($stream));
-                // Call event
-                $event = $this->handler->getEvent('site_maintenance');
-                $response = $event($request, $response);
-                // $this->ucp = $this->configFetcher->getConfig('webcomposer_config.page_not_found');
-                // if (
-                //     $response->getStatusCode() === 200 && // Override only if the supposed response is 200
-                //     $this->playerSession->isLogin() &&
-                //     $this->ucp['currency_mapping'] &&
-                //     isset($this->route['components']['main']) &&
-                //     $this->route['components']['main'] === 'lobby'
-                // ) {
-                //     $currencyMap = explode("\r\n", $this->ucp['currency_mapping']);
-                //     $currency = $this->playerSession->getDetails()['currency'] ?? null;
-                //     if (in_array($currency, $currencyMap)) {
-                //         // Remove processed body stream that came from controller
-                //         $stream = fopen('php://memory', 'r+');
-                //         fwrite($stream, '');
-                //         $response = $response->withBody(new Body($stream));
-                //         // Call event
-                //         $event = $this->handler->getEvent('site_maintenance');
-                //         $response = $event($request, $response);
-                //     }
-                // }
+                $products = explode("\r\n", $this->configs['product_list']);
+                $product = $this->product;
+                $product_key = strtolower($product);
+                $product_key = str_replace(' ', '', $product_key);
+
+                $dates = [
+                    'field_publish_date' => $this->configs['maintenance_publish_date_' . $product_key],
+                    'field_unpublish_date' => $this->configs['maintenance_unpublish_date_' . $product_key],
+                ];
+                if (
+                    in_array($product, $products) &&
+                    $this->isPublished($dates)
+                ) {
+                    // Remove processed body stream that came from controller
+                    $stream = fopen('php://memory', 'r+');
+                    fwrite($stream, '');
+                    $response = $response->withBody(new Body($stream));
+                    // Call event
+                    $event = $this->handler->getEvent('site_maintenance');
+                    $response = $event($request, $response);
+                }
             }
         } catch (\Exception $e) {
             // Do nothing
+        }
+    }
+
+    private function isPublished($data)
+    {
+        if (empty($data['field_publish_date']) && empty($data['field_unpublish_date'])) {
+            return false;
+        } elseif ($data['field_unpublish_date']) {
+            return $data['field_publish_date'] <= strtotime(date('m/d/Y H:i:s')) &&
+                $data['field_unpublish_date'] >= strtotime(date('m/d/Y H:i:s'));
+        } else {
+            return $data['field_publish_date'] <= strtotime(date('m/d/Y H:i:s'));
         }
     }
 }
