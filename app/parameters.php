384af587186d3392f605f7698c6caf495e8c183f
Daniel Tracy Dela Cruz <DanielTracy.DelaCruz@bayviewtechnology.com>
2020-02-19 14:37:30 +0800

    Add cookie service

--- app/parameters.php
+++ app/parameters.php
@@ -12,3 +12,7 @@ $parameters['appsvc.origin.qa2'] = 'http://ctmt-cms-appsvc.games.de1';
 $parameters['appsvc.origin.uat'] = 'http://ctmu-cms-ms.appsvc.stg';
 $parameters['appsvc.origin.stg'] = 'http://cms-ms.appsvc.stg';
 $parameters['appsvc.origin.prd'] = 'http://cms-ms-appsvc.games.twprd';
+$parameters['appsvc.origin.its1'] = 'http://cms-appsvc.games.its1';
+$parameters['appsvc.origin.itct'] = 'http://cms-appsvc.games.itct';
+$parameters['appsvc.origin.istg'] = 'http://cms-appsvc.games.istg';
+$parameters['appsvc.origin.iuat'] = 'http://cms-appsvc.games.iuat';
