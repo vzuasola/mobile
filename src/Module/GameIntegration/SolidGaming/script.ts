04b9181f9534f22cfd6f6ab1c6a20f53de9fd39b
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-03-18 12:15:32 +0800

    Added the viewsFetcher and included the subprovider on launch request for SolidGaming provider

--- src/Module/GameIntegration/SolidGaming/script.ts
+++ src/Module/GameIntegration/SolidGaming/script.ts
@@ -66,6 +66,7 @@ export class SolidGamingModule implements ModuleInterface, GameInterface {
                 data: {
                     product,
                     gameCode: options.code,
+                    subprovider: options.subprovider || undefined,
                     langCode,
                     playMode: true,
                 },
