afce0e89308b53e3ad2e4eeda445c50854cf0059
John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>
2020-07-15 14:03:53 +0800

    [MDAFABET-169] - Initial Commit on Timeout config source change
    Signed-off-by: John Patric Rivera <johnpatric.rivera@bayviewtechnology.com>

--- src/Module/GameIntegration/PAS/script.ts
+++ src/Module/GameIntegration/PAS/script.ts
@@ -189,7 +189,15 @@ export class PASModule implements ModuleInterface, GameInterface {
             // remap language
             const lang = Router.getLanguage();
             const language = this.getLanguageMap(lang);
-
+            // If there is no playerId attachment, this means that there is no session available.
+            if (!this.playerId
+                || (typeof this.pasLoginResponse.errorCode === "boolean" && this.pasLoginResponse.errorCode !== 0)) {
+                this.messageLightbox.showMessage(
+                    this.moduleName,
+                    "unsupported",
+                    options,
+                );
+            }
             if (this.futurama || this.futuramaGold) {
                 const key = this.getKeyByProduct(product);
 
