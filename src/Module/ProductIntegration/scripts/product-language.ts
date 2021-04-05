a2b479eb9549e536cfeaa81d79c7f405364d66b8
Minas Minasyan <minas.minasyan@sportion.net>
2021-04-05 17:16:45 +0400

    [DFMARC-25] - Added new languages on mobile arcade - Bengali-(bn) and Urdu-(pk)

--- src/Module/ProductIntegration/scripts/product-language.ts
+++ src/Module/ProductIntegration/scripts/product-language.ts
@@ -122,6 +122,8 @@ export class  ProductLanguage {
             "bu",
             "lo",
             "km",
+            "bn",
+            "pk",
         ],
         "virtual-sports" : [
             "en",
