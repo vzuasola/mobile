835d5f5757b83b66f18f3cc32678d7eda7dcf9f7
aisrayelyan <anna.israyelyan@sportion.net>
2021-05-07 16:05:49 +0400

    pipeline fix

--- src/Services/Currency/CurrencyTranslation.php
+++ src/Services/Currency/CurrencyTranslation.php
@@ -36,11 +36,10 @@ class CurrencyTranslation
     {
         if (strtoupper($currency) === 'MBC') {
             $currency = 'mBTC';
-        }
-        else if (strtoupper($currency) === 'TUS') {
+        } elseif (strtoupper($currency) === 'TUS') {
             $currency = 'USDT';
         }
-        
+
         switch ($this->lang) {
             case 'sc':
                 if ($translated = Currency::getTranslation($currency)) {
