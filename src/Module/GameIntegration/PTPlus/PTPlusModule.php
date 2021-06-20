2a8ea84503671ad85c2ed1292a94f1af40af75e0
Minas Minasyan <minas.minasyan@sportion.net>
2021-06-20 21:10:20 +0400

    [DFMSODA-12] - PTPlus Playtech Integration

---
+++ src/Module/GameIntegration/PTPlus/PTPlusModule.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\MobileEntry\Module\GameIntegration\PTPlus;
+
+use App\Plugins\ComponentWidget\ComponentModuleInterface;
+
+class PTPlusModule implements ComponentModuleInterface
+{
+}
