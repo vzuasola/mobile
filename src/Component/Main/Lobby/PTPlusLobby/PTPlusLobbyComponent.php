09b3ee21ae0244a3acf818e0a774d26684cc3f1d
mariam <mariam.bagdasaryan@sportion.net>
2021-08-31 13:51:34 +0400

    feture-DFMPTPLUS-9-DF-PTPLus-Mobile-Game-Categories fix ptplus component file

--- src/Component/Main/Lobby/PTPlusLobby/PTPlusLobbyComponent.php
+++ src/Component/Main/Lobby/PTPlusLobby/PTPlusLobbyComponent.php
@@ -5,35 +5,44 @@ use App\Plugins\ComponentWidget\ComponentWidgetInterface;
 
 class PTPlusLobbyComponent implements ComponentWidgetInterface
 {
+   /**
+     * @var App\Fetcher\Drupal\ViewsFetcher
+     */
     private $views;
 
+    /**
+     * @var App\Fetcher\Drupal\ConfigFetcher
+     */
     private $configs;
 
     private $product;
 
+    /**
+     *
+     */
     public static function create($container)
     {
         return new static(
-            $container->get('player_session'),
+            $container->get('views_fetcher'),
             $container->get('config_fetcher'),
-            $container->get('product_resolver'),
-            $container->get('views_fetcher')
+            $container->get('product_resolver')
         );
     }
 
     /**
      * Public constructor
      */
-    public function __construct($playerSession, $configs, $product, $views)
+    public function __construct($views, $configs, $product)
     {
-        $this->playerSession = $playerSession;
-        $this->product = $product;
-        $this->configs = $configs;
         $this->views = $views->withProduct($product->getProduct());
+        $this->product = $product;
+        $this->configs = $configs->withProduct($product->getProduct());
     }
 
     /**
-     * {@inheritdoc}
+     * Defines the template path
+     *
+     * @return string
      */
     public function getTemplate()
     {
@@ -41,16 +50,12 @@ class PTPlusLobbyComponent implements ComponentWidgetInterface
     }
 
     /**
-     * {@inheritdoc}
+     * Defines the data to be passed to the twig template
+     *
+     * @return array
      */
     public function getData()
     {
-        $data = null;
-        try {
-            $data['ptplus'] = $this->views->getViewById('ptplus');
-        } catch (\Exception $e) {
-            $data = [];
-        }
-        return $data;
+        return [];
     }
 }
