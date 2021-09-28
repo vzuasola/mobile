6023ae5ab199093aae297a61b3420cb5bfbd78c0
Ronald Duque <ronald.duque@bayviewtechnology.com>
2021-09-28 14:12:26 +0800

    Add link on components

--- src/Component/Header/HeaderComponent.php
+++ src/Component/Header/HeaderComponent.php
@@ -192,7 +192,7 @@ class HeaderComponent implements ComponentWidgetInterface
         $data['mobile_login_reg'] = $headerConfigs['mobile_login_reg'] ?? 'Login/Join';
         $data['join_now_link'] = $headerConfigs['registration_link'] ?? [];
         $productRoute = $params['route'] ?? '/';
-        $data['logo_link'] = ($product == 'ptplus') ? '/{lang}'.$product : '/{lang}';
+        $data['logo_link'] = '/{lang}';
 
 
         try {
