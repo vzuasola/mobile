09dfe5ac2f3cbc69edfc1ae2f5c8aab98dba7b6a
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2021-11-17 13:29:50 +0800

    fix style

--- src/Component/Main/Lobby/Home/Download/script.ts
+++ src/Component/Main/Lobby/Home/Download/script.ts
@@ -136,14 +136,13 @@ export class DownloadComponent implements ComponentInterface {
         menuClass[2] = "app-download-col-2";
         menuClass[3] = "app-download-col-3";
         menuClass[4] = "col-3";
-        const newItem: any = [];
 
         for (let i = 0; i < downloadItems.length; i++) {
             if (downloadItems.hasOwnProperty(i)) {
                 const itemClass = (typeof menuClass[downloadItems.length] !== "undefined")
                     ? menuClass[downloadItems.length] : "col-3";
                 utility.addClass(downloadItems[i], itemClass);
-                if (i === 3) {
+                if (downloadItems.length === 3) {
                     utility.addClass(downloadItems[i], "push");
                 }
             }
