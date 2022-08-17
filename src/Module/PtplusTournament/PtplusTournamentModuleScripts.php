38306583550b65d5fa949f3d6adbd6bcb9d750a7
Stephanie Aviles <stephanie.aviles@bayviewtechnology.com>
2022-08-17 13:09:55 +0800

    [WEBSITE-482] Add notification prompt if no ptplus account yet, Show login lightbox when tournament is clicked on prelogin

--- src/Module/PtplusTournament/PtplusTournamentModuleScripts.php
+++ src/Module/PtplusTournament/PtplusTournamentModuleScripts.php
@@ -10,23 +10,25 @@ use App\Plugins\ComponentWidget\ComponentAttachmentInterface;
 class PtplusTournamentModuleScripts implements ComponentAttachmentInterface
 {
     private $playerSession;
-
+    private $config;
     /**
      *
      */
     public static function create($container)
     {
         return new static(
-            $container->get('player_session')
+            $container->get('player_session'),
+            $container->get('config_fetcher')
         );
     }
 
     /**
      * Public constructor
      */
-    public function __construct($playerSession)
+    public function __construct($playerSession, $config)
     {
         $this->playerSession = $playerSession;
+        $this->config = $config;
     }
 
     /**
@@ -34,9 +36,22 @@ class PtplusTournamentModuleScripts implements ComponentAttachmentInterface
      */
     public function getAttachments()
     {
+        try {
+            $settings =  $this->config->withProduct('mobile-ptplus')
+                ->getConfig('webcomposer_config.tournament_api_configuration');
+            $api['url'] = $settings['api_url'] ?? "https://ptplus-b.hotspin88.com/loginFromGame?data=";
+            $api['casino'] = $settings['api_casino'] ?? 'goldencircle'; 
+
+        } catch (\Exception $e) {
+            $api['url'] = "https://ptplus-b.hotspin88.com/loginFromGame?data=";
+            $api['casino'] = $settings['api_casino'] ?? 'goldencircle'; 
+        }
+
         return [
             'authenticated' => $this->playerSession->isLogin(),
             'playerId' =>  $this->playerSession->getDetails()['playerId'] ?? '',
+            'apiUrl' => $api['url'],
+            'apiCasino' => $api['casino']
         ];
     }
 }
