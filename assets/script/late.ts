d22646324e3946794a69186d3f93aab3fcd4c9af
Alexandros <alexandros.gougousis@sportion.net>
2023-07-17 17:22:56 +0300

    Remove component to original place.

--- assets/script/late.ts
+++ assets/script/late.ts
@@ -19,7 +19,6 @@ import {MyAccountProfileVerifyPasswordComponent} from "@app/src/Component/Main/M
 import {MyAccountChangePasswordComponent} from "@app/src/Component/Main/MyAccount/ChangePassword/script";
 import {VirtualsLobbyComponent} from "@app/src/Component/Main/Lobby/VirtualsLobby/script";
 import {PTPlusLobbyComponent} from "@app/src/Component/Main/Lobby/PTPlusLobby/script";
-import {LanguageComponent} from "@app/src/Component/Language/script";
 
 console.log("Late script is loaded!");
 // @ts-ignore
@@ -48,7 +47,6 @@ const lateComponents = {
     profile_verify_password: new MyAccountProfileVerifyPasswordComponent(),
     change_password: new MyAccountChangePasswordComponent(),
     ptplus_lobby: new PTPlusLobbyComponent(),
-    language: new LanguageComponent(),
 };
 
 componentManager.logComponents("BEFORE");
