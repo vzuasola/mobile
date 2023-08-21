81db3b411ba0a543656ed08bc43be23b60d81e03
Joao Viera <jvieira@ph.esl-asia.com>
2023-08-21 07:40:58 +0000

    WEBSITE-3484 Adding transition changes to playerGame;

--- src/Module/GameIntegration/CQ9/CQ9ModuleController.php
+++ src/Module/GameIntegration/CQ9/CQ9ModuleController.php
@@ -24,6 +24,8 @@ class CQ9ModuleController
      */
     private $viewsFetcher;
 
+    private $playerGameFetcher;
+
     /**
      *
      */
@@ -34,49 +36,50 @@ class CQ9ModuleController
             $container->get('game_provider_fetcher'),
             $container->get('config_fetcher'),
             $container->get('player'),
-            $container->get('views_fetcher')
+            $container->get('views_fetcher'),
+            $container->get('player_game_fetcher')
         );
     }
 
     /**
      * Public constructor
      */
-    public function __construct($rest, $cq9, $config, $player, $viewsFetcher)
+    public function __construct($rest, $cq9, $config, $player, $viewsFetcher, $playerGameFetcher)
     {
         $this->rest = $rest;
         $this->cq9 = $cq9;
         $this->config = $config->withProduct('mobile-games');
         $this->player = $player;
         $this->viewsFetcher = $viewsFetcher->withProduct('mobile-games');
+        $this->playerGameFetcher = $playerGameFetcher;
     }
 
     /**
-     * @{inheritdoc}
+     * Get GameURL via GetGeneralLobby
      */
-    public function launch($request, $response)
+    public function getGameUrl($request, $requestData)
     {
-        $data['gameurl'] = false;
-        $data['currency'] = false;
-        if ($this->checkCurrency($request)) {
-            $data['currency'] = true;
-            $requestData = $request->getParsedBody();
-            $params = explode('|', $requestData['gameCode']);
+        $data = [
+            'gameurl' => false,
+            'currency' => true,
+        ];
 
-            try {
-                $responseData = $this->cq9->getGameUrlById('icore_cq9', $params[0], [
-                    'options' => [
-                        'languageCode' => $this->languageCode($request),
-                        'providerProduct' => $params[1] ?? null,
-                    ]
-                ]);
-                if ($responseData['url']) {
-                    $data['gameurl'] = $responseData['url'];
-                }
-            } catch (\Exception $e) {
-                $data['currency'] = true;
+        $params = explode('|', $requestData['gameCode']);
+
+        try {
+            $responseData = $this->cq9->getGameUrlById('icore_cq9', $params[0], [
+                'options' => [
+                    'languageCode' => $this->languageCode($request),
+                    'providerProduct' => $params[1] ?? null,
+                ]
+            ]);
+            if ($responseData['url']) {
+                $data['gameurl'] = $responseData['url'];
             }
+        } catch (\Exception $e) {
+            // Do nothing
         }
 
-        return $this->rest->output($response, $data);
+        return $data;
     }
 }
