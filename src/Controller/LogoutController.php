66ee97a81d820689cebece943b41acd2db7a36b6
Alexandros Gougousis <alexandros.gougousis@sportion.net>
2023-10-03 07:33:47 +0000

    Refactor module.
    This refactoring makes easier to replace the duplicated code.

--- src/Controller/LogoutController.php
+++ src/Controller/LogoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\MobileEntry\Controller;
 
+use App\MobileEntry\Tools\DsbCookieHelper;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
@@ -9,21 +10,38 @@ use App\BaseController;
 
 class LogoutController extends BaseController
 {
-    /**
-     *
-     */
     public function view(Request $request, Response $response)
     {
+        $playerSession = $this->get('player_session');
+
         try {
-            $this->get('player_session')->logout();
+            $playerSession->logout();
         } catch (\Exception $e) {
             // do nothing
         }
+
+        $this->resetDafaUrlCookiesIfNeeded($request, $playerSession);
 
         $redirect = $request->getQueryParam('destination') ?: '/';
 
         return $response->withRedirect(
             $this->get('uri')->generateUri($redirect, [])
         );
+    }
+
+    private function resetDafaUrlCookiesIfNeeded($request, $playerSession)
+    {
+        try {
+            $alsConfig = $this->get('config_fetcher')->getConfig('mobile_als.als_configuration');
+        } catch (\Exception $e) {
+            $alsConfig = [];
+        }
+
+        $dsbCookiesAreSet = DsbCookieHelper::dSBCookiesExist();
+        if ($dsbCookiesAreSet) {
+            $tokenParser = $this->get('token_parser');
+            $dsbCookieHelper = new DsbCookieHelper($tokenParser, $playerSession, $alsConfig);
+            $dsbCookieHelper->setDafaUrlCookies();
+        }
     }
 }
--- src/Module/ProductIntegration/ALS/ALSIntegrationModuleController.php
+++ src/Module/ProductIntegration/ALS/ALSIntegrationModuleController.php
@@ -5,6 +5,7 @@ namespace App\MobileEntry\Module\ProductIntegration\ALS;
 use App\Drupal\Config;
 use App\Cookies\Cookies;
 use App\MobileEntry\Services\Product\Products;
+use App\MobileEntry\Tools\DsbCookieHelper;
 use App\Utils\Host;
 
 class ALSIntegrationModuleController
@@ -47,12 +48,6 @@ class ALSIntegrationModuleController
     public function integrate($request, $response)
     {
         try {
-            $isLogin = $this->playerSession->isLogin();
-        } catch (\Exception $e) {
-            $isLogin = false;
-        }
-
-        try {
             $alsConfig = $this->config->getConfig('mobile_als.als_configuration');
         } catch (\Exception $e) {
             $alsConfig = [];
@@ -61,16 +56,13 @@ class ALSIntegrationModuleController
         $enableDomain = $alsConfig['als_enable_domain'] ?? false;
         $url = $alsConfig['als_url'] ?? '';
         $this->generateLobby($url, $enableDomain);
-        $cookies = $alsConfig['als_cookie_url_pre'] ?? '';
-
-        if ($isLogin) {
-            $cookies = $cookies . $alsConfig['als_cookie_url_post'] ?? '';
-        }
 
         Cookies::remove('X_DEVICE_VIEW', [
             'path' => '/'
         ]);
-        $this->setCookie($cookies, $isLogin);
+
+        $dsbCookieHelper = new DsbCookieHelper($this->parser, $this->playerSession, $alsConfig);
+        $dsbCookieHelper->setDafaUrlCookies();
 
         $postData = $request->getParsedBody();
 
@@ -175,49 +167,5 @@ class ALSIntegrationModuleController
         }
 
         return $url;
-    }
-
-    /**
-     *
-     */
-    private function setCookie($cookies, $isLogin)
-    {
-        if ($cookies) {
-            $cookies = $this->parser->processTokens($cookies);
-            $cookies = Config::parse($cookies);
-
-            foreach ($cookies as $key => $value) {
-                $this->createCookie('create', "dafaUrl[$key]", $value);
-            }
-        }
-
-        if (!$isLogin) {
-            $this->createCookie('destroy', 'extToken');
-            $this->createCookie('destroy', 'extCurrency');
-        }
-    }
-
-    /**
-     * Helper function for creating or destroying a cookie
-     */
-    private function createCookie($action, $name, $value = '')
-    {
-        $domain = Host::getDomain();
-
-        $options = [
-            'path' => '/',
-            'domain' => $domain,
-            'secure' => true
-        ];
-
-        if ($action == 'create') {
-            $options['expire'] = 0;
-        }
-
-        if ($action == 'destroy') {
-            $options['expire'] = time() - 3600;
-        }
-
-        Cookies::set($name, $value, $options);
     }
 }
